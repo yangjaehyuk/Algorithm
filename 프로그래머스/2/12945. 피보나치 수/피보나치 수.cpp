ebfb766728077f9c02ed2d1fdf11a369fa09845d
Jaehyuk-Yang <37584686+yangjaehyuk@users.noreply.github.com>
2023-10-13 21:40:46 +0900

    [level 2] Title: 피보나치 수, Time: 0.51 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int arr[100001]={0};
+
+int solution(int n) {
+    int answer = 0;
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++){
+        arr[i]=(arr[i-1]+arr[i-2])%1234567;
+    }
+    return arr[n];
+}
