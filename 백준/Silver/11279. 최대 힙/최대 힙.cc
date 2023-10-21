f618ff1d716901ae9aa5d77560112d0dec35a9f8
Jaehyuk-Yang <37584686+yangjaehyuk@users.noreply.github.com>
2023-10-21 20:28:15 +0900

    [Silver II] Title: 최대 힙, Time: 68 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+priority_queue<int> q;
+int main(){
+
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    int num;
+    cin>>num;
+
+    if(num!=0){
+      q.push(num);
+    }
+
+    else{
+      if(q.empty()){
+        cout<<"0"<<'\n';
+      }
+      else{
+        cout<<q.top()<<'\n';
+        q.pop();
+      }
+    }
+  }
+}
