a9f52fc77dc11a75886752b81bcdabf6897e12b0
Jaehyuk-Yang <37584686+yangjaehyuk@users.noreply.github.com>
2023-10-21 21:14:20 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1001];
+int main(){
+  int n;
+  cin>>n;
+  dp[0]=1;
+  dp[1]=2;
+
+  for(int i=2;i<n;i++){
+    dp[i]=(dp[i-1]+dp[i-2])%10007;
+  }
+
+  cout<<dp[n-1];
+}
