2bfa513a1b0c5587982913f6a0bffc06c7c5a6d2
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-03-09 19:20:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int idx = 0;
+        for(int i=0;i<number.length()-k;i++){
+            int max = 0;
+            for(int j=idx;j<=i+k;j++){
+                if(number.charAt(j) - '0' > max){
+                    max = number.charAt(j) - '0';
+                    idx = j+1;
+                }
+            }
+            answer.append(max);
+        }
+        return answer.toString();
+    }
+}
