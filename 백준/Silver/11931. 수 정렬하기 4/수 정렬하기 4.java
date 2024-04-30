168ce538cef3b40a914f1307846a9aa84d22631a
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-04-30 11:39:49 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1560 ms, Memory: 116828 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		br.close();
+		for(int i=arr.length-1;i>=0;i--) {
+			bw.write(arr[i]+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
