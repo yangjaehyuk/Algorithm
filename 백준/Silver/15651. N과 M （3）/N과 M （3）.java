0d201f9a3be7c9d223c4d42ce9fbb0ab4ac1e912
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-05-07 12:28:21 +0900

    [Silver III] Title: N과 M (3), Time: 692 ms, Memory: 72572 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		arr = new int[n];
+		int[] path = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = i+1;
+		}
+		dfs(0,m,path);
+		System.out.print(sb.toString());
+	}
+	
+	public static void dfs(int lev, int level, int[] path) {
+		if(lev==level) {
+			for(int i=0;i<lev;i++) {
+//				System.out.print(path[i]+" ");
+				sb.append(path[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0;i<arr.length;i++) {
+			path[lev]=arr[i];
+			dfs(lev+1, level, path);
+			
+		}
+	}
+
+}
