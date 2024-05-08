988b16fad74b14c6e1571012745ca29fd635dcd0
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-05-08 09:23:27 +0900

    [Silver III] Title: N과 M (7), Time: 676 ms, Memory: 123580 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] tmp = br.readLine().split(" ");
+		int n = Integer.parseInt(tmp[0]);
+		int m = Integer.parseInt(tmp[1]);
+		arr = new int[n];
+		int[] path = new int[n];
+		String[] docs = br.readLine().split(" ");
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(docs[i]);
+		}
+		Arrays.sort(arr);
+		dfs(0,m,path);
+		System.out.println(sb.toString());
+	}
+	
+	public static void dfs(int lev, int level, int[] path) {
+		if(lev==level) {
+			for(int i=0;i<lev;i++) {
+				sb.append(path[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			path[lev] = arr[i];
+			dfs(lev+1, level, path);
+		}
+	}
+
+}
