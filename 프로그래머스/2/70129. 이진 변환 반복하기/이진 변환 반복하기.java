bd97ba57fabc8fb705bd50b9f37e3550ec2062ca
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-03 01:34:39 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4816.09 ms, Memory: 521 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,26 +1,40 @@
+import java.util.*;
 class Solution {
+    
+    static int cnt = 0;
+    static int zeroCnt = 0;
     public int[] solution(String s) {
         int[] answer = new int[2];
-        int check = 0;
-        int zeroCnt = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            sb.append(s.charAt(i));
+        }
         while(true){
-            if(s.equals("1")) break;
-            else{
-                if(s.contains("0")==true){
-                    int len = s.length();
-                    s = s.replace("0","");
-                    int subLen = s.length();
-                    zeroCnt += len - subLen;
-                    s = Integer.toBinaryString(subLen);
-                    check++;
-                }
-                else{
-                    check++;
-                    s = Integer.toBinaryString(s.length());
+            List<Integer> list = new ArrayList<>();
+            if(sb.toString().equals("1")) break;
+            for(int j=0;j<sb.toString().length();j++){
+                if(sb.toString().charAt(j) == '0'){
+                    zeroCnt++;
+                    list.add(j);
                 }
             }
+            for(int j=0;j<list.size();j++){
+                if(j==0) sb.deleteCharAt(list.get(j));
+                else sb.deleteCharAt(list.get(j)-j);
+            }
+            // System.out.println(zeroCnt);
+            // 0제거후 길이를 2진법으로
+            int size = sb.toString().length();
+            sb.setLength(0);
+            String tmp = Integer.toBinaryString(size);
+            // System.out.println(tmp);
+            for(int j=0;j<tmp.length();j++){
+                sb.append(tmp.charAt(j));
+            }
+            cnt++;
+            
         }
-        answer[0] = check;
+        answer[0] = cnt;
         answer[1] = zeroCnt;
         return answer;
     }
