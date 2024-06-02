5ef78346bf15e868709220c7731a9743d35ad052
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-02 23:32:57 +0900

    [level 2] Title: 올바른 괄호, Time: 27.54 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,18 +3,20 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
-        char[] arr = s.toCharArray();
-        for(int i=0;i<arr.length;i++){
-            if(stack.empty()) stack.push(arr[i]);
-            else{
-                if(stack.peek()=='(' && arr[i]==')'){
+        for(int i=0;i<s.length();i++){
+            stack.add(s.charAt(i));
+            if(stack.size()>1){
+                if(stack.peek()==')' && stack.get(stack.size()-2)=='('){
                     stack.pop();
-                    continue;
+                    stack.pop();
                 }
-                else stack.push(arr[i]);
             }
         }
-        if(stack.size()>0) answer=false;
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        if(stack.size()==0) answer=true;
+        else answer=false;
+
         return answer;
     }
 }
