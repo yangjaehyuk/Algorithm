8fb61a7406f39310a208aea8036451aa1c001c58
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-04 09:19:00 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -3,14 +3,38 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-        while(n != 0){
-            if(n % 2 == 0){
-                n /= 2;
-                continue;
+        if(n==1) return 1;
+        if(n%2==0){
+            while(true){
+                if(n==1) break;
+                if(n%2==0) n/=2;
+                else{
+                    n--;
+                    ans++;
+                    n/=2;
+                }
             }
-            n--;
             ans++;
         }
+        else{
+            n--;
+            ans++;
+            while(true){
+                if(n==1) break;
+                if(n%2==0) n/=2;
+                else{
+                    n--;
+                    ans++;
+                    n/=2;
+                }
+            }
+            ans++;
+            
+        }
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        // System.out.println("Hello Java");
+
         return ans;
     }
 }
