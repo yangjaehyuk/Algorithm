9f6b1a0ec49e86bd7625a80628e9525559c8741a
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-04 20:21:31 +0900

    [level 2] Title: 귤 고르기, Time: 54.21 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,29 +2,36 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<tangerine.length;i++){
-            list.add(tangerine[i]);
-        }
-        Collections.sort(list);
-        for(Integer item: list){
-            map.put(item, map.getOrDefault(item, 0)+1);
-        }
-        List<Integer> tmp = new ArrayList<>();
-        for(Integer key: map.keySet()){
-            tmp.add(map.get(key));
-        }
-        Collections.sort(tmp, Collections.reverseOrder());
-        int sum = 0;
-        int check = 0;
-        for(Integer ans: tmp){
-            if(sum >= k) break;
+            if(map.containsKey(tangerine[i]) == false){
+                map.put(tangerine[i],1);
+            }
             else{
-                sum += ans;
-                check ++;
+                int v = map.get(tangerine[i]);
+                map.put(tangerine[i], v+1);
             }
         }
-        return check;
+        // for(Integer i: map.keySet()){
+        //     System.out.println(i+" "+map.get(i));
+        // }
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, new Comparator<Integer>(){
+            
+        
+             @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(b)-map.get(a);
+            }
+        });
+        int sum = 0;
+        for(Integer i: list){
+            // System.out.println(map.get(i));
+            sum+=map.get(i);
+            answer++;
+            if(sum>=k) break;
+            
+        }
+        return answer;
     }
 }
