f9c1122981c3cb55ebbf163594649c4cd845fca1
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-05 14:19:04 +0900

    [level 2] Title: H-Index, Time: 0.51 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,24 +1,44 @@
 import java.util.*;
 class Solution {
+    // 발표한 논문 n편 중, 
+    // h번 이상 인용된 논문이 h편 이상이고 
+    // 나머지 논문이 h번 이하 인용되었다면 h의 최댓값
+    static int maxi = 0;
     public int solution(int[] citations) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<citations.length;i++){
-            list.add(citations[i]);
+        Arrays.sort(citations);
+        if(citations[0] == citations[citations.length-1]) return 0;
+        for(int i: citations){
+            maxi = Math.max(maxi, i);
         }
-        Collections.sort(list);
-        // 3번 이상 인용된 논문이 3편, 나머진 3번 이하로 인용
-        int maxi = list.get(list.size()-1);
-        for(int i=maxi;i>=0;i--){
-            int cnt = 0;
-            for(int j=0;j<list.size();j++){
-                if(list.get(j) >= i){
-                    cnt++;
+        while(true){
+            int upper = 0;
+            int lower = 0;
+            for(int j=0;j<citations.length;j++){
+                if(citations[j]>=maxi) upper++;
+            }
+            if(upper>=maxi){
+                // h번 이상 인용된 논문이 h편 이상이고
+                for(int k=0;k<citations.length;k++){
+                    if(citations[k]<=maxi){
+                        lower++;
+                    }
+                }
+                if(lower<=maxi){
+                    // 나머지 논문이 h번 이하 인용되었다면 h의 최댓값
+                    answer = maxi;
+                    break;
+                }
+                else{
+                    upper = 0;
+                    lower = 0;
+                    maxi--;
                 }
             }
-            if(cnt >= i){
-                answer = i;
-                break;
+            else{
+                upper = 0;
+                lower = 0;
+                maxi--;
             }
         }
         return answer;
