51fcfc203493e62efa048fb662b0b5a5c058e64b
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-05 09:42:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.23 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,37 +2,23 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        char[] arr = s.toCharArray();
-        int idx = 0;
-        while(true){
-            if(idx == s.length()) break;
-            StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
             Stack<Character> stack = new Stack<>();
-            for(int i=0;i<s.length();i++){
-                sb.append(arr[(idx+i) % s.length()]);
-            }
-            for(int j=0;j<sb.toString().length();j++){
-                if(stack.size() == 0) stack.add(sb.toString().charAt(j));
+            for(int j=0;j<s.length();j++){
+                if(stack.isEmpty()){
+                    stack.add(s.charAt((i+j)%s.length()));
+                }
                 else{
-                    if(stack.peek() == '(' && sb.toString().charAt(j) == ')'){
+                    if((stack.peek()=='[' && s.charAt((i+j)%s.length()) == ']') || (stack.peek()=='{' && s.charAt((i+j)%s.length())=='}') || (stack.peek()=='(' && s.charAt((i+j)%s.length())==')')){
                         stack.pop();
+                        continue;
                     }
-                    else{
-                        if(stack.peek() == '{' && sb.toString().charAt(j) == '}'){
-                            stack.pop();
-                        }
-                        else if(stack.peek() == '[' && sb.toString().charAt(j) == ']'){
-                            stack.pop();
-                        }
-                        else{
-                            stack.add(sb.toString().charAt(j));
-                        }
-                    }
+                    else stack.add(s.charAt((i+j)%s.length()));
                 }
             }
-            if(stack.size() == 0) answer++;
-            System.out.println(stack.size());
-            idx++;
+            // System.out.println(stack.isEmpty());
+            if(stack.isEmpty()) answer++;
+            
         }
         return answer;
     }
