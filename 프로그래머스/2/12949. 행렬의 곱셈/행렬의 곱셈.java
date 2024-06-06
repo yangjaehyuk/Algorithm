f50cf5db7763dba7f13c64f44b20ade73e903a93
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-06 20:53:14 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.31 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,22 +1,23 @@
 import java.util.*;
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
-        List<List<Integer>> list = new ArrayList<>();
+        int[][] answer = {};
+        answer = new int[arr1.length][arr2[0].length];
         for(int i=0;i<arr1.length;i++){
-            List<Integer> tmp = new ArrayList<>();
             for(int j=0;j<arr2[0].length;j++){
                 int sum = 0;
-                for(int k=0;k<arr1[0].length;k++){
-                    sum += arr1[i][k]*arr2[k][j];
+                for(int k=0;k<arr2.length;k++){
+                    // for(int l=0;l<arr2.length;l++){
+                    //     int tmp = arr1[i][j] * arr2[l][k];
+                    //     System.out.println(tmp);
+                    // }
+                    int tmp = arr1[i][k] * arr2[k][j];
+                    sum += tmp;
+                    // System.out.println(tmp);
+                    // System.out.println(i+" "+k+" "+j);
                 }
-                tmp.add(sum);
-            }
-            list.add(new ArrayList<>(tmp));
-        }
-        for(int i=0;i<list.size();i++){
-            for(int j=0;j<list.get(i).size();j++){
-                answer[i][j] = list.get(i).get(j);
+                // System.out.println(sum);
+                answer[i][j] = sum;
             }
         }
         return answer;
