2d12a4659d31219fe41522900f1111faee3ba7b4
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-06 21:46:11 +0900

    [level 2] Title: 프로세스, Time: 0.75 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,34 +1,51 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        List<Integer> lp = new ArrayList<>();
         for(int i=0;i<priorities.length;i++){
-            list.add(priorities[i]);
+            lp.add(priorities[i]);
         }
-        int idx = 0;
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        lp.sort(new Comparator<>(){
+           public int compare(Integer i1, Integer i2){
+               return i2 - i1;
+           } 
+        });
+        int answer = 0;
+        for(int i=0;i<priorities.length;i++){
+            map.put(i, priorities[i]);
+        }
+        int step = 1;
+        int idx = -1;
+        for(Integer i: map.keySet()){
+            idx++;
+            if(map.get(i)==lp.get(0)){
+                ans.add(i);
+                break;
+            }
+        }
+        // System.out.println(idx); // 시작 인덱스
+        int lpIdx = 1;
         while(true){
-            if(Collections.max(list) == 0) break;
+            if(step==priorities.length) break;
             else{
-                int max = Collections.max(list);
-                if(list.get(idx) == max){
-                    queue.add(idx);
-                    list.set(idx, 0);
-                }
-                else{
-                    if(idx == list.size()-1) idx = 0;
-                    else idx++;
+                idx++;
+                if(lp.get(lpIdx) == priorities[idx%priorities.length]){
+                    ans.add(idx%priorities.length);
+                    lpIdx++;
+                    step++;
                 }
             }
         }
-        int index = 1;
-        for(Integer item: queue){
-            if(item == location){
-                answer = index;
+        int check = 0;
+        for(Integer i2: ans){
+            check++;
+            if(i2 == location){
+                answer = check;
                 break;
-            }                
-            else index ++;
+            }
         }
         return answer;
     }
