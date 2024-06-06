382f374b9ed9e598be8077825e3ea1a3e947182f
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-06 16:41:21 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,45 +1,37 @@
 import java.util.*;
 class Solution {
-    public static int cnt = 1;
-//     public void dfs(int lev, int level, List<Integer> list, int[] path, boolean[] visited){
-//         int tmp = 1;
-//         if(lev == level){
-//             for(int i=0;i<lev;i++){
-//                 tmp *= path[i];
-//             }
-//             cnt += tmp;
-//             return;
-//         }
-//         for(int i=lev;i<list.size();i++){
-//             if(visited[i] == false){
-//                 visited[i] = true;
-//                 path[lev] = list.get(i);
-//                 dfs(lev+1, level, list, path, visited);
-//                 visited[i] = false;
-//             }
-//         }        
-        
-//     }
     public int solution(String[][] clothes) {
         int answer = 0;
-        HashMap<String, Integer> map = new HashMap<>();
-        // int[] path = new int[31];
-        // boolean[] visited = new boolean[31];
-        // List<Integer> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         for(int i=0;i<clothes.length;i++){
             if(map.containsKey(clothes[i][1])){
-                map.put(clothes[i][1],map.get(clothes[i][1])+1);
+                int val = map.get(clothes[i][1]);
+                map.put(clothes[i][1], val+1);
             }
-            else map.put(clothes[i][1], 1);
+            else{
+                map.put(clothes[i][1], 1);
+            }
+            // System.out.println(map.containsKey(clothes[i][1]));
+            
         }
-        for(Integer key: map.values()){
-            // list.add(key);
-            cnt *= key+1;
+        // System.out.println(map.size());
+        if(map.size()==1){
+            return map.get(clothes[0][1]);
         }
-        // for(int i=1;i<=list.size();i++){
-        //     dfs(0, i, list, path, visited);
-        // }
-        
-        return cnt - 1;
+        else{
+            int sum = 0;
+            for(String s: map.keySet()){
+                int tmp = 0;
+                tmp += map.get(s)+1;
+                if(sum == 0){
+                    sum = tmp;
+                }
+                else{
+                    sum *= tmp;
+                }
+            }
+            answer = sum - 1;
+        }
+        return answer;
     }
 }
