26793be5fc6fa47cfc38f59e7c05de7c47dc2e48
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-07 15:39:59 +0900

    [level 2] Title: 전화번호 목록, Time: 219.00 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,23 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        String tmp = "a";
-        Arrays.sort(phone_book);
-        for(String item: phone_book){
-            if(item.startsWith(tmp)){
-                return false;
-            }
-            tmp = item;
+        boolean answer = false;
+        List<String> phones = new ArrayList<>();
+        for(String s: phone_book){
+            phones.add(s);
         }
-        return true;
+        Collections.sort(phones);
+        String tmp = "A";
+        for(int i=0;i<phones.size();i++){
+            if(phones.get(i).startsWith(tmp) == false){
+                tmp = phones.get(i);
+                answer = true;
+            }
+            else{
+                answer = false;
+                break;
+            }
+        }
+        return answer;
     }
 }
