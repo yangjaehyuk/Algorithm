92b94dbcc2e54bb46ff0faf0314b574bf517b103
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-08 17:10:09 +0900

    [level 3] Title: 네트워크, Time: 6.95 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,44 @@
 import java.util.*;
 class Solution {
+    static int[][] graph;
+    static boolean[] visited;
+    static int cnt = 0;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(visited[i] == false){
-                q.add(i);
-                visited[i] = true;
-                while(!q.isEmpty()){
-                    int start = q.poll();
-                    for(int j=0;j<n;j++){
-                        if(visited[j] == false && computers[start][j] == 1){
-                            q.add(j);
-                            visited[j] = true;
-                        }
-                    }
+        graph = new int[n][n];
+        visited = new boolean[n];
+        
+        
+        for(int i=0;i<computers.length;i++){
+            for(int j=0;j<computers[i].length;j++){
+                if(i==j) continue;
+                if(computers[i][j] == 1 && computers[i][j] == computers[j][i]){
+                    graph[i][j] = 1;
+                    graph[j][i] = graph[i][j];
                 }
-                answer++;
             }
-            
         }
-        return answer;
+        
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(i);
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+    
+    public static void dfs(int start){
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].length;j++){
+                if(i==j) continue;
+                if(graph[start][i] == 1 && graph[i][start] == 1 && !visited[i]){
+                    visited[i] = true;
+                    dfs(i);
+                }
+            }
+        }
     }
 }
