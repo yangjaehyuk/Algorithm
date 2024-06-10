8fdd44c7b60bb4e4c1f2edf7a7ca53b2d098f2de
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-06-10 16:32:23 +0900

    [level 2] Title: 주식가격, Time: 53.55 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        List<Integer> ans = new ArrayList<>();
+        int[] answer = {};
+        answer = new int[prices.length];
         for(int i=0;i<prices.length-1;i++){
             Stack<Integer> stack = new Stack<>();
             for(int j=i+1;j<prices.length;j++){
-                if(prices[i] <= prices[j]){
+                if(prices[i]<=prices[j]){
                     stack.add(prices[j]);
                 }
                 else{
@@ -14,13 +14,10 @@ class Solution {
                     break;
                 }
             }
-            if(stack.size() == 0) ans.add(1);
-            else ans.add(stack.size());
+            answer[i] = stack.size();
         }
-        ans.add(0);
-        for(int i=0;i<prices.length;i++){
-            answer[i]=ans.get(i);
-        }
+        
+        answer[prices.length-1] = 0;
         return answer;
     }
 }
