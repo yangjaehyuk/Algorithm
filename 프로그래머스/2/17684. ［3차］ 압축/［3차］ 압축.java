84444f9a7bc4796f190586b14ac8b5231647273c
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-01 22:50:28 +0900

    [level 2] Title: [3차] 압축, Time: 0.55 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,35 +1,51 @@
 import java.util.*;
 class Solution {
-    public static int idx = 0;
     public int[] solution(String msg) {
         int[] answer = {};
         List<Integer> list = new ArrayList<>();
-        int num = 65;
-        int plus = 27;
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=1;i<27;i++){
-            char alpha = (char)num;
-            map.put(String.valueOf(alpha), i);
-            num++;
+        Map<String, Integer> alpha = new HashMap<>();
+        int idx = 65;
+        for(int i=0;i<26;i++){
+            char tmp = (char)idx;
+            String keys = String.valueOf(tmp);
+            alpha.put(keys, i+1);
+            idx++;
         }
+        
+        int next = 27;
+        int alphaIdx = 0;
         while(true){
-            int print = 0;
+            boolean flag = true;
             StringBuilder sb = new StringBuilder();
-            if(idx == msg.length()) break;
-            for(int i=idx;i<msg.length();i++){
-                sb.append(msg.charAt(i));
-                if(map.containsKey(sb.toString()) == true){
-                    print = map.get(sb.toString());
-                    idx++;
-                }
-                else break;
+            sb.append(msg.charAt(alphaIdx));
+            if(alphaIdx == msg.length()-1 && flag){
+                list.add(alpha.get(sb.toString()));
+                break;
             }
-            map.put(sb.toString(), plus);
-            plus++;
-            list.add(print);
-            print=0;
-            sb.setLength(0);
+            if(alpha.containsKey(sb.toString())){
+                while(true){
+                    if(alphaIdx == msg.length()-1){
+                        list.add(alpha.get(sb.toString()));
+                        flag = false;
+                        break;
+                    }
+                    alphaIdx++;
+                    sb.append(msg.charAt(alphaIdx));
+                    if(!alpha.containsKey(sb.toString())){
+                        alpha.put(sb.toString(), next);
+                        next++;
+                        list.add(alpha.get(sb.toString().substring(0, sb.toString().length()-1)));
+                        sb.setLength(0);
+                        break;
+                    }
+                    
+                }
+                if(!flag) break;
+                
+            }
+            
         }
+                
         answer = new int[list.size()];
         for(int i=0;i<list.size();i++){
             answer[i] = list.get(i);
