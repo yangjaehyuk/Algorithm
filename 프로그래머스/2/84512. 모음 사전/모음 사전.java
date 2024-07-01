716a75a0a43a29ef84b9fdcb48b7f3efe9b9386c
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-01 23:27:19 +0900

    [level 2] Title: 모음 사전, Time: 3.64 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,33 +1,22 @@
 import java.util.*;
 class Solution {
-    static int num = 0;
-    static char[] path = new char[5];
-    static char[] tmp = {'A', 'E', 'I', 'O', 'U'};
-    static ArrayList<String> list;
+    static List<String> list = new ArrayList<>();
+    static Character[] arr = {'A','E','I','O','U'};
     public int solution(String word) {
         int answer = 0;
-        list = new ArrayList<>();
-        for(int i=1;i<=5;i++){
-            char[] path = new char[5];
-            dfs(tmp, word, 0, i, path);
-        }
-        Collections.sort(list);
-        answer = list.indexOf(word)+1;
+        dfs(0,5,"",word);
+        answer = list.indexOf(word);
         return answer;
     }
-    public void dfs(char[] tmp, String word, int lev, int level, char[] path){
-        StringBuilder sb = new StringBuilder();
+    
+    public static void dfs(int lev, int level, String tmp, String word){
+        list.add(tmp);
         if(lev==level){
-            num++;
-            for(int i=0;i<lev;i++){
-                sb.append(path[i]);
-            }
-            list.add(sb.toString());
             return;
         }
-        for(int i=0;i<5;i++){
-            path[lev] = tmp[i];
-            dfs(tmp, word, lev+1, level, path);
+        
+        for(int i=0;i<arr.length;i++){
+            dfs(lev+1, level, tmp+arr[i], word);
         }
     }
 }
