d6566ebc58e9be33124bb652f410ec53dbdc6f47
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-02 10:51:47 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 22.48 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,53 +1,41 @@
 import java.util.*;
 class Solution {
-    public static int[] dx = {1,0,1};
-    public static int[] dy = {1,1,0};
     public int solution(int m, int n, String[] board) {
+        int[] dx = {1,0,1,0};
+        int[] dy = {0,1,1,0};
         int answer = 0;
         char[][] arr = new char[m][n];
+        int step = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 arr[i][j] = board[i].charAt(j);
             }
         }
         while(true){
+            step++;
+            if(step==300) break;
             boolean[][] visited = new boolean[m][n];
-            boolean flag = false;
-            for(int i=0;i<board.length;i++){
-                for(int j=0;j<board[i].length();j++){
-                    int cnt = 0;
-                    for(int k=0;k<3;k++){
-                        int nx = i+dx[k];
-                        int ny = j+dy[k];
-                        if(nx >= 0 && nx < board.length && ny >= 0 && ny < board[i].length()){
-                            if(arr[nx][ny] != ' ' && arr[nx][ny] == arr[i][j]){
-                                cnt++;
-                                if(cnt == 3){
-                                    visited[i][j] = true;
-                                    for(int l=0;l<3;l++){
-                                        int nowx = i+dx[l];
-                                        int nowy = j+dy[l];
-                                        visited[nowx][nowy] = true;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
             for(int i=0;i<m;i++){
                 for(int j=0;j<n;j++){
-                    if(visited[i][j]) arr[i][j] = ' ';
-                }
-            }
-            for (int i = board.length - 1; i >= 0; i--) {
-                for (int j = 0; j < board[i].length(); j++) {
-                    if (arr[i][j] == ' ') {
-                        for (int k = i - 1; k >= 0; k--) {
-                            if (arr[k][j] != ' ') {
-                                arr[i][j] = arr[k][j];
-                                arr[k][j] = ' ';
+                    boolean flag = true;
+                    int cnt = 0;
+                    for(int k=0;k<4;k++){
+                        int nx = i+dx[k];
+                        int ny = j+dy[k];
+                        if(nx >=0 && nx < m && ny>=0 && ny<n){
+                            if(arr[i][j] != arr[nx][ny] || arr[i][j] == ' '){
+                                flag = false;
                                 break;
+                            }
+                            else cnt++;
+                        }
+                    }
+                    if(flag && cnt == 4){
+                        for(int l=0;l<4;l++){
+                            int nx = i+dx[l];
+                            int ny = j+dy[l];
+                            if(nx >= 0 && nx < m && ny >= 0 && ny < n){
+                                visited[nx][ny] = true;
                             }
                         }
                     }
@@ -56,18 +44,31 @@ class Solution {
             for(int i=0;i<m;i++){
                 for(int j=0;j<n;j++){
                     if(visited[i][j] == true){
-                        flag = true;
-                        break;
+                        answer++;
+                        arr[i][j] = ' ';
                     }
                 }
             }
-            for(int i=0;i<m;i++){
+            for(int i=m-1;i>=0;i--){
                 for(int j=0;j<n;j++){
-                    if(visited[i][j]) answer++;
+                    if(arr[i][j] == ' '){
+                        for(int k=i-1;k>=0;k--){
+                            if (arr[k][j] != ' ') {
+                                    arr[i][j] = arr[k][j];
+                                    arr[k][j] = ' ';
+                                    break;
+                            }
+                        }
+                    }
                 }
             }
-            if(flag == false) break;
         }
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
         return answer;
     }
 }
