74de9a641f0a0bbc0119621fa7af9d9d0a20cd31
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-02 20:58:33 +0900

    [level 2] Title: 땅따먹기, Time: 22.14 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,17 +1,30 @@
-import java.util.*;
 class Solution {
+    static int maxi = 0;
     int solution(int[][] land) {
         int answer = 0;
-        int[][] dp = new int[land.length+1][land[0].length];
-        for(int i=1;i<=land.length;i++){
-            for(int j=0;j<land[0].length;j++){
-                for(int k=0;k<land[0].length;k++){
-                    if(j==k) continue;
-                    dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][k]);
-                    answer = Math.max(dp[i][j], answer);
+        int m = land.length;
+        int n = land[0].length;
+        int idx = 0;
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<n;i++){
+            dp[0][i] = land[0][i];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    if(j == k) continue;
+                    dp[i][j] = Math.max(dp[i-1][k] + land[i][j], dp[i][j]);
+                }
+                
+            }
+            
+            
+        }
+        for(int j=0;j<m;j++){
+                for(int k=0;k<n;k++){
+                    maxi = Math.max(maxi, dp[j][k]);
                 }
             }
-        }
-        return answer;
+        return maxi;
     }
 }
