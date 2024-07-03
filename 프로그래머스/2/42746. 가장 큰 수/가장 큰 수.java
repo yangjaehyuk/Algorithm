1c86ebe5bc0b381efddec0a7c7023b26b3eed268
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-03 09:40:14 +0900

    [level 2] Title: 가장 큰 수, Time: 1.93 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,16 +1,28 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        StringBuilder answer = new StringBuilder();
-        List<String> tmp = new ArrayList<>();
-        for(int i: numbers){
-            tmp.add(String.valueOf(i));
+        String answer = "";
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<numbers.length;i++){
+            list.add(String.valueOf(numbers[i]));
         }
-        Collections.sort(tmp, (o1, o2) -> (o2+o1).compareTo(o1+o2));
-        for(String item: tmp){
-            answer.append(item);
+        Collections.sort(list, new Comparator<>(){
+           @Override
+            public int compare(String s1, String s2){
+                return Integer.parseInt(s2+s1) - Integer.parseInt(s1+s2); 
+            }
+        });
+        for(String s: list){
+            answer += s;
         }
-        if(answer.charAt(0)=='0') return "0";
-        return answer.toString();
+        
+        int cnt = 0;
+        
+        for(int i=0;i<answer.length()-1;i++){
+            if(answer.charAt(i) == answer.charAt(i+1) && answer.charAt(i) == '0') cnt++;
+        }
+        
+        if(cnt == answer.length() - 1) return "0";
+        return answer;
     }
 }
