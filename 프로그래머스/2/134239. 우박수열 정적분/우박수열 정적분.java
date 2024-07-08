fc8c1c8f5daf1c63c99ce491d6258162f07e77e0
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-08 16:45:48 +0900

    [level 2] Title: 우박수열 정적분, Time: 9.93 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -2,48 +2,73 @@ import java.util.*;
 class Solution {
     public double[] solution(int k, int[][] ranges) {
         double[] answer = {};
-        answer = new double[ranges.length];
-        List<Integer> list = new ArrayList<>();
         List<Double> ans = new ArrayList<>();
-        List<pair> now = new ArrayList<>();
-        int n = 0;
-        list.add(k);
-        while(k!=1){
-            if(k%2==0) k/=2;
-            else k=k*3+1;
-            list.add(k);
-        }
-        n = list.size();
-        for(int i=0;i<list.size();i++){
-            now.add(new pair(i, list.get(i)));
-        }
-        for(int i=0;i<now.size()-1;i++){
-            int miny = Math.min(now.get(i).y, now.get(i+1).y);
-            int maxy = Math.max(now.get(i).y, now.get(i+1).y);
-            double square = miny;
-            double triangle = (maxy - miny) / 2.0;
-            ans.add(square + triangle);
-        }
-        for(int i=0;i<ranges.length;i++){
-            double sum = 0.0;
-            int x1 = ranges[i][0];
-            int x2 = n - Math.abs(ranges[i][1]) - 1;
-            if(x1 > x2) answer[i] = -1;
+        Map<Long, Integer> map = new HashMap<>();
+        List<Double> list = new ArrayList<>();
+        map.put(0L, k);
+        long idx = 1;
+        while(k != 1){
+            if(k % 2 == 0){
+                k /= 2;
+                map.put(idx, k);
+                idx++;
+            }
             else{
-                for(int j=x1;j<x2;j++){
-                    sum += ans.get(j);
-                }
-                answer[i] = sum;
+                k *= 3;
+                k++;
+                map.put(idx, k);
+                idx++;
             }
         }
-        return answer;
-    }
-    public static class pair{
-        int x;
-        int y;
-        pair(int x, int y){
-            this.x = x;
-            this.y = y;
+        long n = idx - 1;
+        for(long i = 0;i < map.size() - 1;i++){
+            double mini = (double)Math.min(map.get(i), map.get(i+1));
+            double maxi = (double)Math.max(map.get(i), map.get(i+1));
+            double size = mini + (maxi - mini) / 2;
+            list.add(size);
         }
+        for(int i=0;i<ranges.length;i++){
+            int[] tmp = ranges[i];
+            int x = tmp[0];
+            int y = tmp[1];
+            double tmpv = 0.0;
+            if(x == 0 && y == 0){
+                for(int j=0;j<list.size();j++){
+                    tmpv += list.get(j);
+                }
+                ans.add(tmpv);
+            }
+            else if(y <= 0){
+                long tmpY = n - Math.abs(y);
+                if(x > tmpY) ans.add(-1.0);
+                else{
+                    if(x == tmpY) ans.add(0.0);
+                    else{
+                        for(int j=x;j<tmpY;j++){
+                            tmpv += list.get(j);
+                        }
+                        ans.add(tmpv);
+                    }
+                }
+            }
+            else if(x >= 0 && y >= 0){
+                if(x == y) ans.add(0.0);
+                else{
+                    if(x < y) {
+                        for(int j=x;j<y;j++){
+                            tmpv += list.get(j);
+                        }
+                        ans.add(tmpv);
+                    }
+                    else ans.add(-1.0);
+                }
+            }
+            
+        }
+        answer = new double[ans.size()];
+        for(int i=0;i<ans.size();i++){
+            answer[i] = ans.get(i);
+        }
+        return answer;
     }
 }
