6339fc4dac872704eaecde12bd958f9aa7deb351
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-07-09 09:43:22 +0900

    [level 2] Title: 삼각 달팽이, Time: 152.93 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,45 +1,76 @@
 import java.util.*;
 class Solution {
-    public static final int[] dx={0,1,-1};
-    public static final int[] dy={1,0,-1};
-    // 대각선 위, 아래, 오른
     public int[] solution(int n) {
+        List<Integer> ans = new ArrayList<>();
         int[] answer = {};
-        int tmp = 1;
-        for(int i=1;i<=n;i++){
-            tmp+=i;
-        }
-        int num = 1;
-        int x = 0;
-        int y = 0;
-        int d = 0;
-        answer = new int[tmp-1];
-        if(n==1){
-            answer[0] = 1;
-            return answer;
-        }
-        int[][] arr = new int[n][n];
-        while(true){
-            arr[y][x] = num++;
-            int nx = x+dx[d];
-            int ny = y+dy[d];
-            if(nx == n || ny == n || arr[ny][nx] != 0){
-                d = (d+1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-                if(arr[ny][nx] != 0 || nx == n || ny == n) break;
-            }
-            y = ny;
-            x = nx;
-        }
-        int idx = 0;
+        int[][] map = new int[n + 1][n + 1];
+        int start = 1;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j] != 0){
-                    answer[idx] = arr[i][j];
-                    idx++;
+            for(int j=0;j<=i;j++){
+                if(j == 0){
+                    map[i][j] = start++;
+                }
+                else if(i == n - 1){
+                    map[i][j] = start++;
                 }
             }
+        }
+        int x = n - 1;
+        int y = n - 1;
+        if(n == 3){
+            answer = new int[6];
+            int[] tmp = {1,2,6,3,4,5};
+            for(int i=0;i<6;i++){
+                answer[i] = tmp[i];
+            }
+            return answer;
+        }
+        while(true){
+            boolean check = true;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<=i;j++){
+                    if(map[i][j] == 0){
+                        check = false;
+                        break;
+                    }
+                }
+            }
+            if(check) break;
+            while(true){
+                x--;
+                y--;
+                if(map[x][y] != 0){
+                    x++;
+                    y++;
+                    break;
+                }
+                map[x][y] = start++;
+            }
+            while(true){
+                x++;
+                if(map[x][y] != 0){
+                    x--;
+                    break;
+                }
+                map[x][y] = start++;
+            }
+            while(true){
+                y++;
+                if(map[x][y] != 0){
+                    y--;
+                    break;
+                }
+                map[x][y] = start++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                ans.add(map[i][j]);
+            }
+        }
+        answer = new int[ans.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = ans.get(i);
         }
         return answer;
     }
