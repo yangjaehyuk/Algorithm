5d2e9c0733c115141e081846e2964139d24f69c2
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-05 01:16:39 +0900

    [level 1] Title: 폰켓몬, Time: 6.04 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 2.45 ms
+메모리: 79.4 MB, 시간: 6.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 16:23:23
+2024년 09월 05일 01:16:37
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int answer = 0;
-        int n = nums.length;
-        for(int i=0;i<n;i++){
+        int maxi=nums.length/2;
+        Set<Integer> set=new HashSet();
+        for(int i=0;i<nums.length;i++){
             set.add(nums[i]);
         }
-        int[] arr = new int[set.size()];
-        int idx=0;
-        for(int num: set){
-            arr[idx++]=num;
-        }
-        int len = arr.length;
-        if(len>n/2) answer=n/2;
-        else if(len<=n/2) answer=len;
-        return answer;
+        if(set.size()>=maxi) return maxi;
+        return set.size();
     }
 }
