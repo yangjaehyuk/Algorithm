3f20a02ba5f157de3a8e44e17e464e87fc800652
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-05 15:48:36 +0900

    [level 2] Title: 카펫, Time: 12.85 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 0.03 ms
+메모리: 72.8 MB, 시간: 12.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 03일 23:14:03
+2024년 09월 05일 15:48:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,31 +1,21 @@
 import java.util.*;
 class Solution {
-    static int ans1 = 0;
-    static int ans2 = 0;
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int sum = brown + yellow;
-        for(int i=sum;i>=1;i--){
-            if(i<sum/i) break;
-            if(sum%i==0){
-                // System.out.println(i+" "+sum/i);
-                // int width = i;
-                // int height = sum/i;
-                int tmpw = i*2;
-                if(tmpw>brown) continue;
-                else{
-                    int tmph = yellow/(i-2);
-                    if(tmpw + tmph * 2 == brown){
-                        ans1 = i;
-                        ans2 = sum/i;
-                        break;
-                    } 
+        int total = brown + yellow;
+        for(int i=1;i<=total;i++){
+            for(int j=1;j<=i;j++){
+                if(i*j==total){
+                    int garo = i;
+                    int sero = j;
+                    if((garo-2) * (sero-2) == yellow){
+                        answer[0] = garo;
+                        answer[1] = sero;
+                        return answer;
+                    }
                 }
-               
             }
         }
-        answer[0] = ans1;
-        answer[1] = ans2;
         return answer;
     }
 }
