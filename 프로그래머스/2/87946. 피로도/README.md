167ea3d697d0ba329e510ba469e7179c6427143d
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-08 00:49:18 +0900

    [level 2] Title: 피로도, Time: 99.27 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
 
 ### 성능 요약
 
-메모리: 70.9 MB, 시간: 0.66 ms
+메모리: 94.6 MB, 시간: 99.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 09:38:54
+2024년 09월 08일 00:49:12
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,59 +1,47 @@
 import java.util.*;
 class Solution {
-    static int maxi = 0;
     static int[] path;
     static boolean[] visited;
     static int[] arr;
+    static int answer = 0;
+    static int maxi = 0;
     public int solution(int k, int[][] dungeons) {
-        // "최소 필요 피로도"와 던전 탐험을 마쳤을 때 소모되는 "소모 피로도"가 있습니다. 
-        // "최소 필요 피로도"는 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도를 나타내며, 
-        // "소모 피로도"는 던전을 탐험한 후 소모되는 피로도를 나타냅니다. 
-        // 예를 들어 "최소 필요 피로도"가 80, "소모 피로도"가 20인 던전을 탐험하기 위해서는 유저의 현재 남은 피로도는 80 이상 이어야 하며, 
-        // 던전을 탐험한 후에는 피로도 20이 소모됩니다.
-        // k는 유저의 현재 필요도
-        // 최소 필요 필요도, 소모 피로도가 담긴 2차원 배열
-        int answer = -1;
         arr = new int[dungeons.length];
-        for(int i=1;i<=dungeons.length;i++){
-            arr[i-1] = i;
-        }
         path = new int[dungeons.length];
         visited = new boolean[dungeons.length];
-        dfs(0, dungeons.length, dungeons, k);
-        answer = maxi;
+        for(int i=0;i<dungeons.length;i++){
+            arr[i] = i;
+        }
+        dfs(k, 0, dungeons.length, dungeons);
         return answer;
     }
-    
-    public static void dfs(int lev, int level, int[][] dungeons, int now){
-        int cnt = 0;
+    static void dfs(int k, int lev, int level, int[][] dungeons){
         if(lev == level){
-            List<Integer> list = new ArrayList<>();
-            for(int i=0;i<lev;i++){
-                // System.out.print(path[i]+" ");
-                
-                list.add(path[i]);
-            }
-            for(Integer i: list){
-                int idx = i-1;
-                if(now >= dungeons[idx][0]){
-                    now -= dungeons[idx][1];
-                    cnt++;
+            int tmp = k;
+            for(int j=0;j<lev;j++){
+                if(tmp >= dungeons[path[j]][0]){
+                    tmp -= dungeons[path[j]][1];
+                    maxi++;
                 }
-                else break;
             }
-            maxi = Math.max(maxi, cnt);
-            // System.out.println();
+            if(maxi >= answer) {
+                answer = maxi;
+                maxi = 0;
+            }
+            else maxi = 0;
+            
+            System.out.println();
             return;
         }
-        
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<level;i++){
             if(!visited[i]){
-                visited[i] = true;
                 path[lev] = arr[i];
-                dfs(lev+1, level, dungeons, now);
+                visited[i] = true;
+                dfs(k, lev+1, level, dungeons);
                 visited[i] = false;
                 path[lev] = 0;
             }
         }
+        
     }
 }
