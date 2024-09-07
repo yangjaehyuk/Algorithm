c7ab5be24e886cf46a5f49901f9b2ca7970c0db5
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-08 02:02:28 +0900

    [level 2] Title: 기능개발, Time: 0.91 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,51 +2,67 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
+        Stack<Integer> stack = new Stack();
         List<Integer> list = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<progresses.length;i++){
-            int day = 0;
-            int progress = progresses[i];
+        for(int i=0;i<speeds.length;i++){
+            int now = progresses[i];
             int speed = speeds[i];
-            while(true){
-                if(progress>=100){
-                    break;
+            if(stack.isEmpty()){
+                int tmp = 0;
+                while(true){
+                    if(now >= 100) break;
+                    now+=speed;
+                    tmp++;
                 }
-                else{
-                    progress += speed;
-                    day++;
-                }
-            }
-            list.add(day);
-        }
-        int tmp = 0;
-        for(int i=0;i<list.size();i++){
-            if(tmp == 0){
-                tmp = list.get(i);
-                ans.add(tmp);
+                stack.add(tmp);
             }
             else{
-                if(tmp < list.get(i)){
-                    tmp = list.get(i);
-                    ans.add(tmp);
+                int cri = stack.peek();
+                int tmp = 0;
+                while(true){
+                    if(now >= 100) break;
+                    now+=speed;
+                    tmp++;
                 }
-                else ans.add(tmp);
+                    
+                if(cri >= tmp){
+                    stack.add(cri);
+                }
+                else{
+                    stack.add(tmp);
+                }
             }
         }
-        Set<Integer> set = new HashSet<>(ans);
-        List<Integer> setL = new ArrayList<>(set);
-        Collections.sort(setL);
-        answer = new int[set.size()];
-        int idx = 0;
-        for(Integer it: setL){
-            int tm = 0;
-            for(Integer i: ans){
-                if(it==i){
-                    tm++;
+        while(!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+        Set<Integer> set = new HashSet(list);
+        List<Integer> li = new ArrayList(set);
+        Collections.sort(list);
+        Collections.sort(li);
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<li.size();i++){
+            int cnt = 0;
+            for(int j=0;j<list.size();j++){
+                if(li.get(i) == list.get(j)) {
+                    cnt++;
+                    if(j==list.size()-1){
+                        ans.add(cnt);
+                    }
+                }
+                else{
+                    if(cnt==0) continue;
+                    else{
+                        ans.add(cnt);
+                        cnt=0;
+                    }
                 }
             }
-            answer[idx] = tm;
-            idx++;
+           
+        }
+        answer = new int[ans.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = ans.get(i);
         }
         return answer;
     }
