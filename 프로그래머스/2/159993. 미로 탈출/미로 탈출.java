669c34d2d875acf2d74292489d9be8697fde5284
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-15 01:14:45 +0900

    [level 2] Title: 미로 탈출, Time: 6.07 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,94 +1,91 @@
 import java.util.*;
 class Solution {
-    static int startx;
-    static int starty;
-    static int leverx;
-    static int levery;
-    static int endx;
-    static int endy;
+    static boolean[][] visited;
+    static char[][] arr;
+    static int[][] ans;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static char[][] map;
-    static int[][] load;
     public int solution(String[] maps) {
-        int answer = 0;
-        boolean check1 = true;
-        boolean check2 = true;
-        map = new char[maps.length][maps[0].length()];
-        load = new int[maps.length][maps[0].length()];
-        for(int i=0;i<maps.length;i++){
-            for(int j=0;j<maps[i].length();j++){
-                map[i][j] = maps[i].charAt(j);
-                if(map[i][j] == 'S'){
+        int n = maps.length;
+        int m = maps[0].length();
+        int startx = 0;
+        int starty = 0;
+        int rebelx = 0;
+        int rebely = 0;
+        int endx = 0;
+        int endy = 0;
+        ans = new int[n][m];
+        arr = new char[n][m];
+        visited = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j] = maps[i].charAt(j);
+                if(maps[i].charAt(j) == 'S'){
                     startx = i;
                     starty = j;
                 }
-                else if(map[i][j] == 'L'){
-                    leverx = i;
-                    levery = j;
+                else if(maps[i].charAt(j) == 'L'){
+                    rebelx = i;
+                    rebely = j;
                 }
-                else if(map[i][j] == 'E'){
+                else if(maps[i].charAt(j) == 'E'){
                     endx = i;
                     endy = j;
                 }
             }
         }
-        bfs1(startx, starty);
-        if(load[leverx][levery] == 0){
-            answer = -1;
+        boolean checkRebel = false;
+        boolean checkEnd = false;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(startx, starty));
+        visited[startx][starty] = true;
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int x = p.x;
+            int y = p.y;
+            if(x == rebelx && y == rebely) {
+                checkRebel = true;
+                break;
+            }
+            for(int i=0;i<4;i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && arr[nx][ny] != 'X'){
+                    q.add(new Pair(nx, ny));
+                    visited[nx][ny] = true;
+                    ans[nx][ny] = ans[x][y] + 1;
+                }
+            }
         }
-        else{
-            bfs2(leverx, levery);
-            if(load[endx][endy] == 0) answer = -1;
-            else answer = load[endx][endy];
+        if(checkRebel){
+            while(!q.isEmpty()) q.poll();
+            q.add(new Pair(rebelx, rebely));
+            boolean[][] visited2 = new boolean[n][m];
+            visited2[rebelx][rebely] = true;
+            while(!q.isEmpty()){
+                Pair p = q.poll();
+                int x = p.x;
+                int y = p.y;
+                if(x == endx && y == endy) {
+                    checkEnd = true;
+                    break;
+                }
+                for(int i=0;i<4;i++){
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited2[nx][ny] && arr[nx][ny] != 'X'){
+                        q.add(new Pair(nx, ny));
+                        visited2[nx][ny] = true;
+                        ans[nx][ny] = ans[x][y] + 1;
+                    }
+                }
+            }
         }
+        else return -1;
+        int answer = 0;
+        if(checkEnd) answer = ans[endx][endy];
+        else return -1;
         return answer;
-    }
-    
-    public static void bfs1(int startx, int starty){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(startx, starty));
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int x = p.x;
-            int y = p.y;
-            visited[x][y] = true;
-            if(x == leverx && y == levery) return;
-            for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length && map[nx][ny] != 'X' && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    q.add(new Pair(nx, ny));
-                    load[nx][ny] = load[x][y] + 1;
-                    
-                }
-            }
-        }
-    }
-    
-    public static void bfs2(int startx, int starty){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(startx, starty));
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int x = p.x;
-            int y = p.y;
-            visited[x][y] = true;
-            if(x == endx && y == endy) return;
-            for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length && map[nx][ny] != 'X' && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    q.add(new Pair(nx, ny));
-                    load[nx][ny] = load[x][y] + 1;
-                    
-                }
-            }
-        }
     }
     
     public static class Pair{
