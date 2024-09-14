82b6d01923827cc176050c3ac81b963d32708527
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-15 02:02:02 +0900

    [level 2] Title: 피보나치 수, Time: 3.62 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,16 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int[] dp = new int[n+1];
-        if(n==1) answer = 1;
-        else{
-            dp[0] = 0;
-            dp[1] = 1;
-            for(int i=2;i<=n;i++){
-                dp[i] = (dp[i-1] + dp[i-2])%1234567;
-            }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<n;i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
-        return dp[n];
+        return dp[n - 1];
     }
 }
