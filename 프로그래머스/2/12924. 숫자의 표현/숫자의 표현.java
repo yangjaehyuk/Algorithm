eca290a4f268658030dc0c5a94b9550f675181c8
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-29 19:04:38 +0900

    [level 2] Title: 숫자의 표현, Time: 1.13 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,19 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        for(int i=1;i<=n/2;i++){
-            int tmp=i;
-            for(int j=i+1;j<=n/2+1;j++){
-                tmp+=j;
-                if(tmp==n){
-                    answer++;
-                    break;
-                }
-                else if(tmp>n) break;
+        int answer = 0;
+        int divn = n/2 + 1;
+        for(int i=1;i<divn;i++){
+            int sum = i;
+            for(int j=i+1;j<=divn;j++){
+                sum += j;
+                if(sum == n) answer++;
+                else if(sum > n) break;
             }
         }
-        return answer;
-        
+        return answer + 1;
     }
 }
