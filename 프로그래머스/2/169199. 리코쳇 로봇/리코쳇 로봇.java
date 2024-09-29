8b4e19c20d3fe71707d45ebbbafc2ae9beda144e
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-29 20:55:06 +0900

    [level 2] Title: 리코쳇 로봇, Time: 18.36 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,79 +1,64 @@
 import java.util.*;
 class Solution {
-    static int[][] ans;
-    static int m;
-    static int n;
-    static int startx;
-    static int starty;
-    static int finalx;
-    static int finaly;
-    static char[][] arr;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public int solution(String[] board) {
         int answer = 0;
-        m = board.length;
-        n = board[0].length();
-        arr = new char[m][n];
-        ans = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j] = board[i].charAt(j);
-                ans[i][j] = 123456789;
-            }
-        }
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j] == 'R'){
+        int startx = 0;
+        int starty = 0;
+        int endx = 0;
+        int endy = 0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length();j++){
+                if(board[i].charAt(j) == 'R'){
                     startx = i;
                     starty = j;
                 }
-                
-                else if(arr[i][j] == 'G'){
-                    finalx = i;
-                    finaly = j;
+                else if(board[i].charAt(j) == 'G'){
+                    endx = i;
+                    endy = j;
+                    
                 }
             }
         }
+        int[][] graph = new int[board.length][board[0].length()];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length();j++){
+                graph[i][j] = 123456789;
+            }
+        }
+        graph[startx][starty] = 0;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(startx, starty));
-        ans[startx][starty] = 0;
         while(!q.isEmpty()){
             Pair p = q.poll();
             int x = p.x;
             int y = p.y;
-            for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for(int j=0;j<4;j++){
+                int nx = x;
+                int ny = y;
                 while(true){
-                    if(nx >= 0 && nx < m && ny >= 0 && ny < n && arr[nx][ny] != 'D'){
-                        nx += dx[i];
-                        ny += dy[i];
+                    if(nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length() && board[nx].charAt(ny) != 'D'){
+                        nx += dx[j];
+                        ny += dy[j];
                     }
                     else{
-                        nx -= dx[i];
-                        ny -= dy[i];
+                        nx -= dx[j];
+                        ny -= dy[j];
                         break;
                     }
                 }
-                if(ans[nx][ny] > ans[x][y] + 1){
-                    ans[nx][ny] = ans[x][y] + 1;
-                    q.add(new Pair(nx, ny));    
+                if (graph[x][y] + 1 < graph[nx][ny]) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    q.add(new Pair(nx, ny));
                 }
+                
             }
-            
         }
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         System.out.print(ans[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        if(ans[finalx][finaly] == 123456789) return -1;
-        else return ans[finalx][finaly];
+        if(graph[endx][endy] == 123456789) answer= -1;
+        else answer = graph[endx][endy];
+        return answer;
     }
-    
     public static class Pair{
         int x;
         int y;
