5c3e071135da6143470bd09c5594e5c39709ac33
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-29 19:40:22 +0900

    [level 3] Title: 네트워크, Time: 14.79 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,35 @@
-import java.util.*;
 class Solution {
     static int[][] graph;
     static boolean[] visited;
-    static int cnt = 0;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        graph = new int[n][n];
-        visited = new boolean[n];
-        
-        
+        graph = new int[computers.length][computers[0].length];
+        visited = new boolean[computers.length];
         for(int i=0;i<computers.length;i++){
             for(int j=0;j<computers[i].length;j++){
-                if(i==j) continue;
-                if(computers[i][j] == 1 && computers[i][j] == computers[j][i]){
+                if(computers[i][j] == 1 && computers[j][i] == 1 && i != j){
                     graph[i][j] = 1;
-                    graph[j][i] = graph[i][j];
+                    graph[j][i] = 1;
                 }
             }
         }
         
-        for(int i=0;i<n;i++){
+        
+        for(int i=0;i<computers.length;i++){
             if(!visited[i]){
                 visited[i] = true;
                 dfs(i);
-                cnt++;
+                answer++;
             }
         }
-        
-        return cnt;
+        return answer;
     }
     
     public static void dfs(int start){
         for(int i=0;i<graph.length;i++){
             for(int j=0;j<graph[i].length;j++){
                 if(i==j) continue;
-                if(graph[start][i] == 1 && graph[i][start] == 1 && !visited[i]){
+                if(graph[start][i] ==1 && graph[i][start]==1 && !visited[i]){
                     visited[i] = true;
                     dfs(i);
                 }
