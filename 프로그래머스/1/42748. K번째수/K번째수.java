a698e68b45098cb82d7944816d685eee460b8dda
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-09-29 21:12:43 +0900

    [level 1] Title: K번째수, Time: 0.28 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,23 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        List<Integer> ans = new ArrayList<>();
-        for(int[] command: commands){
+        int[] answer = new int[commands.length];
+        for(int i=0;i<commands.length;i++){
             List<Integer> list = new ArrayList<>();
-            int start = command[0];
-            int end = command[1];
-            int cri = command[2]-1;
-            for(int i=start-1;i<end;i++){
-                list.add(array[i]);
+            for(int j=commands[i][0] - 1;j<commands[i][1];j++){
+                list.add(array[j]);
             }
             Collections.sort(list);
-            // System.out.println(list.get(cri));
-            ans.add(list.get(cri));
-        }
-        answer = new int[ans.size()];
-        for(int i=0;i<ans.size();i++){
-            answer[i]=ans.get(i);
+            // for(Integer item: list){
+            //     System.out.println(item+" ");
+            // }
+            answer[i] = list.get(commands[i][2] - 1);
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 70.5 MB, 시간: 0.24 ms
+메모리: 77.4 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 22:53:18
+2024년 09월 29일 21:12:38
 
 ### 문제 설명
 
