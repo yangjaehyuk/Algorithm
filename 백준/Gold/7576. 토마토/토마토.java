30bdb08fe3bcc99d46ce51d0c683a5b1796e7a35
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-02 00:14:24 +0900

    [Gold V] Title: 토마토, Time: 772 ms, Memory: 143412 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,80 +1,93 @@
+/******************************************************************************
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
 import java.util.*;
-public class Main {
-	public static int maxi = 0;
-	public static int[][] arr;
-	public static int[] dx = {1,0,0,-1};
-	public static int[] dy = {0,-1,1,0};
-	public static void main(String[] args) throws Exception{
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main
+{
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] docs = br.readLine().split(" ");
 		int n = Integer.parseInt(docs[0]);
 		int m = Integer.parseInt(docs[1]);
-		List<Pair> list = new ArrayList<Pair>();
 		arr = new int[m][n];
-		boolean[][] visited = new boolean[m][n];
-		for(int i=0;i<m;i++) {
-			String[] temp = br.readLine().split(" ");
-			for(int j=0;j<temp.length;j++) {
-				arr[i][j] = Integer.parseInt(temp[j]);
-			}
+		visited = new boolean[m][n];
+		for(int i=0;i<m;i++){
+		    String[] tmp = br.readLine().split(" ");
+		    for(int j=0;j<tmp.length;j++){
+		        arr[i][j] = Integer.parseInt(tmp[j]);
+		    }
+		}
+		boolean flag = true;
+		for(int i=0;i<m;i++){
+		    for(int j=0;j<n;j++){
+		        if(arr[i][j] == 0){
+		            flag = false;
+		            break;
+		        }
+		    }
+		}
+		if(flag){
+		    System.out.println(0);
+		    return;
+		}
+		Queue<Pair> q = new LinkedList<>();
+		for(int i=0;i<m;i++){
+		    for(int j=0;j<n;j++){
+		        if(arr[i][j] == 1){
+		            q.add(new Pair(i, j));
+		            visited[i][j] = true;
+		        }
+		    }
 		}
 		
-		for(int i=0;i<m;i++) {
-			for(int j=0;j<n;j++) {
-				if(arr[i][j] == 1) {
-					list.add(new Pair(i,j));
-				}
-			}
+		while(!q.isEmpty()){
+		    Pair p = q.poll();
+		    int x = p.x;
+		    int y = p.y;
+		    for(int i=0;i<4;i++){
+		        int nx = x + dx[i];
+		        int ny = y + dy[i];
+		        if(nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny] && arr[nx][ny] == 0){
+		            q.add(new Pair(nx, ny));
+		            visited[nx][ny] = true;
+		            arr[nx][ny] = arr[x][y] + 1;
+		        }
+		    }
 		}
-		Queue<Pair> q = new LinkedList<Pair>();
-		for(Pair p: list) {
-			q.add(new Pair(p.x, p.y));
-			visited[p.x][p.y] = true;
-		}
-		while(!q.isEmpty()) {
-			Pair pa = q.poll();
-			int x = pa.x;
-			int y = pa.y;
-			for(int k=0;k<4;k++) {
-				int nx = x+dx[k];
-				int ny = y+dy[k];
-				if(nx>=0 && ny>=0 && nx<m & ny<n && !visited[nx][ny] && arr[nx][ny] == 0) {
-					arr[nx][ny] = arr[x][y]+1;
-					visited[nx][ny] = true;
-					q.add(new Pair(nx,ny));
-				}
-			}
+		int maxi = 0;
+		for(int i=0;i<m;i++){
+		    for(int j=0;j<n;j++){
+		        maxi = Math.max(maxi, arr[i][j]);
+		        if(arr[i][j] == 0) {
+		            System.out.println(-1);
+		            return;
+		        }
+		    }
 		}
 		
-//		for(int i=0;i<m;i++) {
-//			for(int j=0;j<n;j++) {
-//				System.out.print(arr[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
+		System.out.println(maxi - 1);
 		
-		for(int i=0;i<m;i++) {
-			for(int j=0;j<n;j++) {
-				if(arr[i][j] == 0) {
-					System.out.println(-1);
-					System.exit(0);
-				}
-				else maxi = Math.max(maxi, arr[i][j]);
-			}
-		}
-		System.out.println(maxi-1);
+		
 	}
 	
 	public static class Pair{
-		int x;
-		int y;
-		
-		Pair(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
+	    int x;
+	    int y;
+	    Pair(int x, int y){
+	        this.x = x;
+	        this.y = y;
+	    }
 	}
 }
