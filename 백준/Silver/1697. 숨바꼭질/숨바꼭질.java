c7b227f3cc225edefa37b620b5a78d4e4da8d15c
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-03 01:48:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 18824 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,39 +1,49 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
-	public static int[] dx = {1,-1,2};
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] docs = br.readLine().split(" ");
-		int start= Integer.parseInt(docs[0]);
-		int end = Integer.parseInt(docs[1]);
-		int[] arr = new int[200001];
-		boolean[] visited = new boolean[200001];
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(start);
-		visited[start] = true;
-		while(!q.isEmpty()) {
-			int x = q.poll();
-			if(x+dx[0]<=100000 && !visited[x+dx[0]]) {
-				arr[x+dx[0]] = arr[x]+1;
-				q.add(x+dx[0]);
-				visited[x+dx[0]] = true;
-			}
-			if(x+dx[1]>=0 && !visited[x+dx[1]]) {
-				arr[x+dx[1]] = arr[x]+1;
-				q.add(x+dx[1]);
-				visited[x+dx[1]] = true;
-				
-			}
-			if(x*dx[2]<=100000 && !visited[x*dx[2]]) {
-				arr[x*dx[2]] = arr[x]+1;
-				q.add(x*dx[2]);
-				visited[x*dx[2]] = true;
-			}
-		}
-		System.out.println(arr[end]);
-	}
+    public static int w;
+    public static int h;
+    public static int[] dx = {1, 1, 2};
+    public static int[] arr;
+    public static boolean[] visited;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] docs = br.readLine().split(" ");
+        int start = Integer.parseInt(docs[0]);
+        int end = Integer.parseInt(docs[1]);
+        arr = new int[100001];
+        visited = new boolean[100001];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        arr[start] = 0;
+        visited[start] = true;
+        
+        while(!q.isEmpty()){
+            int p = q.poll();
+            if(p + dx[0] < 100001 && !visited[p + dx[0]]){
+                visited[p + dx[0]] = true;
+                arr[p + dx[0]] = arr[p] + 1;
+                q.add(p + dx[0]);
+            }
+            
+            if(p - dx[1] >= 0 && !visited[p - dx[1]]){
+                visited[p - dx[1]] = true;
+                arr[p - dx[1]] = arr[p] + 1;
+                q.add(p - dx[1]);
+            }
+            
+            if(p * dx[2] <= 100000 && !visited[p * dx[2]]){
+                visited[p * dx[2]] = true;
+                arr[p * dx[2]] = arr[p] + 1;
+                q.add(p * dx[2]);
+            }
+        }
+        
+        
+        System.out.println(arr[end]);
+        
+    }
+    
 }
