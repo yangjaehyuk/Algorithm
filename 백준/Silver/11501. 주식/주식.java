b7a2bf779e82782962d33ef805c9a0f4cd067a67
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-03 22:27:09 +0900

    [Silver II] Title: 주식, Time: 1096 ms, Memory: 368448 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,27 +1,29 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		for(int i=0;i<n;i++) {
-			List<Long> list = new ArrayList<Long>();
-			int num = Integer.parseInt(br.readLine());
-			long sum = 0;
-			long maxi = 0;
-			String[] tmp = br.readLine().split(" ");
-			for(String item: tmp) {
-				list.add(Long.parseLong(item));
-			}
-			for(int j=list.size()-1;j>=0;j--) {
-				if(maxi<list.get(j)) maxi = list.get(j);
-				else {
-					sum+=maxi-list.get(j);
-				}
-			}
-			System.out.println(sum);
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for(int i=0;i<num;i++){
+            List<Integer> list = new ArrayList<>();
+            int len = Integer.parseInt(br.readLine());
+            String[] docs = br.readLine().split(" ");
+            for(int j=0;j<docs.length;j++){
+                list.add(Integer.parseInt(docs[j]));
+            }
+            long maxi = 0;
+            long sum = 0;
+            for(int j=list.size() - 1;j >= 0;j--){
+                
+                if(maxi < list.get(j)) maxi = list.get(j);
+                else{
+                    sum += maxi - list.get(j);
+                }
+            }
+            System.out.println(sum);
+            
+        }
+    }
 }
