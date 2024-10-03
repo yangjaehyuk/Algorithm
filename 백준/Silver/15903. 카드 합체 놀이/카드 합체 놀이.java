5c14507c46c678c93536283d8caf7e747d316b65
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-03 23:16:48 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 140 ms, Memory: 15208 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -1,28 +1,29 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tmp = br.readLine().split(" ");
-		int n = Integer.parseInt(tmp[0]);
-		int m = Integer.parseInt(tmp[1]);
-		long sum = 0;
-		String[] value = br.readLine().split(" ");
-		PriorityQueue<Long> q = new PriorityQueue<Long>();
-		for(String item: value) {
-			q.offer(Long.parseLong(item));
-		}
-		for(int i=0;i<m;i++) {
-			long tmpV = q.poll()+q.poll();
-			q.offer(tmpV);
-			q.offer(tmpV);
-		}
-		for(Long item: q) {
-			sum+=item;
-		}
-		System.out.println(sum);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] docs = br.readLine().split(" ");
+        int n = Integer.parseInt(docs[0]);
+        int m = Integer.parseInt(docs[1]);
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        String[] tmp = br.readLine().split(" ");
+        for(int i=0;i<n;i++){
+            pq.offer(Long.parseLong(tmp[i]));
+        }
+        for(int i=0;i<m;i++){
+            long first = pq.poll();
+            long second = pq.poll();
+            long res = first + second;
+            pq.offer(res);
+            pq.offer(res);
+        }
+        long res = 0;
+        while(!pq.isEmpty()){
+            res += pq.poll();
+        }
+        System.out.println(res);
+    }
 }
