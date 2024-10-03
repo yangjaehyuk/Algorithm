c5cc7e81be6ac2f794c0549f9d92d290a382d00c
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-04 00:23:05 +0900

    [Silver I] Title: 회의실 배정, Time: 592 ms, Memory: 52580 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,54 +1,50 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		List<Pair> list = new ArrayList<Pair>();
-		int cnt = 0;
-		for(int i=0;i<n;i++) {
-			String[] tmp = br.readLine().split(" ");
-			int s = Integer.parseInt(tmp[0]);
-			int e = Integer.parseInt(tmp[1]);
-			Pair p = new Pair(s,e);
-			list.add(p);
-		}
-		Collections.sort(list, new Comparator<Pair>() {
 
-			@Override
-			public int compare(Pair p1, Pair p2) {
-				// TODO Auto-generated method stub
-				if(p1.end==p2.end) return p1.start-p2.start;
-				return p1.end - p2.end;
-			}
-		});
-		List<Pair> ans = new ArrayList<Pair>();
-//		for(Pair p: list) {
-//			System.out.println(p.start+" "+p.end);
-//		}
-		for(int i=0;i<list.size();i++) {
-			if(i==0) {
-				ans.add(new Pair(list.get(i).start, list.get(i).end));
-				cnt++;
-			}
-			else {
-				if(ans.get(ans.size()-1).end<=list.get(i).start) {
-					ans.add(new Pair(list.get(i).start, list.get(i).end));
-					cnt++;
-				}
-			}
-		}
-		System.out.println(cnt);
-	}
-	
-	public static class Pair{
-		int start;
-		int end;
-		Pair(int start, int end){
-			this.start = start;
-			this.end = end;
-		}
-	}
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        List<Pair> list = new ArrayList<>();         
+        for (int i = 0; i < num; i++) {             
+            String[] tmp = br.readLine().split(" ");             
+            int first = Integer.parseInt(tmp[0]);             
+            int second = Integer.parseInt(tmp[1]);             
+            list.add(new Pair(first, second));         
+        }
+        Collections.sort(list, new Comparator<Pair>() {             
+            @Override             
+            public int compare(Pair x, Pair y) {
+                if(x.y==y.y) return x.x-y.x;
+                return x.y - y.y;
+            }         
+        });
+        // for(Pair p: list){
+        //     System.out.println(p.x+" "+p.y);
+        // }
+        
+        int end = list.get(0).y;
+        int cnt = 1;
+        
+        for(int i=1;i<num;i++){
+           if(end <= list.get(i).x){
+               end = list.get(i).y;
+               cnt++;
+           }
+        }
+        
+        System.out.println(cnt);
+        
+    }
+    
+    public static class Pair{
+        int x;
+        int y;
+        Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
 }
