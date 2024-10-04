632315f2447105beae3e3e5f3f6066d2b6221d4f
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-04 22:38:15 +0900

    [Gold V] Title: 선 긋기, Time: 1424 ms, Memory: 329548 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -1,60 +1,54 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		List<Pair> list = new ArrayList<Pair>();
-		List<Pair> ans = new ArrayList<Pair>();
-		int line = 0;
-		for(int i=0;i<n;i++) {
-			String[] tmp = br.readLine().split(" ");
-			int x = Integer.parseInt(tmp[0]);
-			int y = Integer.parseInt(tmp[1]);
-			list.add(new Pair(x,y));
-		}
-		Collections.sort(list, new Comparator<Pair>() {
 
-			@Override
-			public int compare(Pair p1, Pair p2) {
-				// TODO Auto-generated method stub
-				return p1.x - p2.x;
-			}
-		});
-		
-//		for(Pair p: list) {
-//			System.out.println(p.x+" "+p.y);
-//		}
-		for(Pair p:list) {
-			if(ans.isEmpty()) {
-				ans.add(new Pair(p.x,p.y));
-			}
-			else {
-				Pair last = ans.get(ans.size() - 1);
-				if (p.x <= last.y) {
-                    last.y = Math.max(last.y, p.y);
-                } else {
-                    ans.add(new Pair(p.x, p.y));
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        List<Pair> list = new ArrayList<>();
+        for(int i=0;i<num;i++){
+            String[] docs = br.readLine().split(" ");
+            list.add(new Pair(Integer.parseInt(docs[0]), Integer.parseInt(docs[1])));
+        }
+        Collections.sort(list, new Comparator<Pair>() {
+           @Override
+           public int compare(Pair p1, Pair p2){
+               return p1.x - p2.x;
+           }
+            
+        });
+        
+        // for(Pair p: list){
+        //     System.out.println(p.x + " " + p.y);
+        // }
+        
+        int sum = 0;
+        int end = list.get(0).y;
+        int start = list.get(0).x;
+        for(int i=1;i<num;i++){
+            if(end < list.get(i).x){
+                sum += end - start;
+                start = list.get(i).x;
+                end = list.get(i).y;
+            }
+            else{
+                if(end < list.get(i).y){
+                    end = list.get(i).y;
                 }
-			}
-		}
-		
-		for(Pair p: ans) {
-			line+=p.y-p.x;
-		}
-		System.out.println(line);
-	}
-	public static class Pair{
-		int x;
-		int y;
-		Pair(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	
-	
+            }
+        }
+        sum += end - start;
+        System.out.println(sum);
+    }
+    
+    public static class Pair{
+        int x;
+        int y;
+        Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
 }
