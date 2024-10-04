3b81cc1a355b915ce095d85c9571469ea62403f7
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-04 23:38:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1268 ms, Memory: 73956 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,37 +1,35 @@
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int[] presum;
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] docs = br.readLine().split(" ");
         int n = Integer.parseInt(docs[0]);
         int m = Integer.parseInt(docs[1]);
+        // m개 이하
         String[] tmp = br.readLine().split(" ");
-        arr = new int[n + 1];
-        presum = new int[n + 1];
-        for(int i=1; i<=n; i++){
-            arr[i] = Integer.parseInt(tmp[i - 1]);
+        int[] list = new int[n + 1];
+        int[] pre = new int[n + 1];
+        for(int i=1;i<=n;i++){
+            list[i] = Integer.parseInt(tmp[i - 1]);
         }
         int tmpv = 0;
-        for(int i=1; i<=n; i++){
-            tmpv = tmpv + arr[i];
-            presum[i] = tmpv;
+        for(int i=1;i<=n;i++){
+            tmpv = tmpv + list[i];
+            pre[i] = tmpv;
         }
-        for(int i=0; i<m; i++){
-            String[] res = br.readLine().split(" ");
-            int start = Integer.parseInt(res[0]);
-            int end = Integer.parseInt(res[1]);
-            int sum = presum[end] - presum[start - 1];
+        for(int i=0;i<m;i++){
+            String[] temp = br.readLine().split(" ");
+            int start = Integer.parseInt(temp[0]);
+            int end = Integer.parseInt(temp[1]);
+            int sum = pre[end] - pre[start - 1];
             bw.flush();
             bw.write(sum + "\n");
         }
         bw.close();
     }
+    
+    
 }
