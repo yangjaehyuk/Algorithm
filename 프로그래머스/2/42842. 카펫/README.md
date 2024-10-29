f6ba96cbcd4712a673b6666f746c264528f56526
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-10-29 18:14:51 +0900

    [level 2] Title: 카펫, Time: 6.87 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 12.85 ms
+메모리: 76.2 MB, 시간: 6.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 05일 15:48:33
+2024년 10월 29일 18:14:49
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        int[] answer = {};
+        answer = new int[2];
+        boolean flag = false;
         int total = brown + yellow;
+        
         for(int i=1;i<=total;i++){
+            if(flag) break;
+            int ygaro = i - 2;
             for(int j=1;j<=i;j++){
-                if(i*j==total){
-                    int garo = i;
-                    int sero = j;
-                    if((garo-2) * (sero-2) == yellow){
-                        answer[0] = garo;
-                        answer[1] = sero;
-                        return answer;
+                if(i * j == total){
+                    if(ygaro * (j - 2) == yellow) {
+                        answer[0] = i;
+                        answer[1] = j;
+                        flag = true;
                     }
                 }
             }
