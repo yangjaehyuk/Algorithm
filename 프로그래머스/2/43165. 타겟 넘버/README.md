acb3b5eed404780acf972d89100e6b311fb64606
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2024-11-01 18:26:16 +0900

    [level 2] Title: 타겟 넘버, Time: 59.23 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 87.8 MB, 시간: 73.28 ms
+메모리: 74.8 MB, 시간: 59.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 15일 01:57:23
+2024년 11월 01일 18:26:14
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,32 @@
 import java.util.*;
 class Solution {
+    static char[] arr = {'-', '+'};
     static char[] path;
-    static char[] cal = {'-', '+'};
-    static int answer = 0;
+    static int answer;
     public int solution(int[] numbers, int target) {
         path = new char[numbers.length];
-        dfs(0, numbers, target);
+        dfs(0, numbers.length, numbers, target);
         return answer;
     }
     
-    public static void dfs(int lev, int[] numbers, int target){
-        int level = numbers.length;
+    public static void dfs(int lev, int level, int[] numbers, int target){
         if(lev == level){
             int sum = 0;
             for(int i=0;i<lev;i++){
-                if(path[i]=='-') sum -= numbers[i];
-                else sum += numbers[i];
+                if(path[i] == '-'){
+                    sum -= numbers[i];
+                }
+                else{
+                    sum += numbers[i];
+                }
             }
             if(sum == target) answer++;
             return;
         }
         
-        for(int i=0;i<cal.length;i++){
-            path[lev] = cal[i];
-            dfs(lev+1, numbers, target);
+        for(int i=0;i<2;i++){
+            path[lev] = arr[i];
+            dfs(lev+1, level, numbers, target);
         }
     }
 }
