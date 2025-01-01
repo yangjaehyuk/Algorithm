66a70c23b6438a94b7e65daceb66ce8100559951
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-01 16:11:06 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 2.88 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 2.32 ms
+메모리: 88.1 MB, 시간: 2.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 29일 16:49:06
+2025년 01월 01일 16:11:04
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,23 +2,15 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
-        String[] ans = s.split("\\s");
-        // for(int i=0;i<ans.length;i++){
-        //     System.out.println(ans[i]);
-        // }
-        for(int i=0;i<ans.length;i++){
-            list.add(Integer.parseInt(ans[i]));
+        List<Integer> lst = new ArrayList<>();
+        String[] arr = s.split(" ");
+        for(int i=0;i<arr.length;i++){
+            lst.add(Integer.parseInt(arr[i]));
         }
-        // for(Integer i: list){
-        //     System.out.println(i);
-        // }
-        Collections.sort(list);
-        sb.append(list.get(0));
-        sb.append(" ");
-        sb.append(list.get(list.size()-1));
-        answer = sb.toString();
+        Collections.sort(lst);
+        answer += lst.get(0);
+        answer += ' ';
+        answer += lst.get(lst.size()-1);
         return answer;
     }
 }
