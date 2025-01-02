fbfc6ccc45792fccf46d42e56e19098d810e6a8f
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-02 18:20:57 +0900

    [level 2] Title: 피보나치 수, Time: 2.46 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 4.37 ms
+메모리: 88 MB, 시간: 2.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 29일 17:39:44
+2025년 01월 02일 18:20:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if(n == 0) answer = 0;
-        else if(n == 1) answer = 1;
-        int[] arr = new int[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
-        for(int i=2;i<=n;i++){
-            arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
+        int[] dp = new int[100001];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int j=2;j<=n;j++){
+            dp[j] = (dp[j-1] + dp[j-2]) % 1234567;
         }
-        return arr[n];
+        answer = dp[n];
+        return answer;
     }
 }
