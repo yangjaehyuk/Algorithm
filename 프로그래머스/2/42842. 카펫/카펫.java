b6306ff14a88390ffba82456266e9bf302910da6
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-02 20:26:12 +0900

    [level 2] Title: 카펫, Time: 896.15 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,22 +3,26 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
         answer = new int[2];
-        boolean flag = false;
         int total = brown + yellow;
-        
-        for(int i=1;i<=total;i++){
-            if(flag) break;
-            int ygaro = i - 2;
-            for(int j=1;j<=i;j++){
-                if(i * j == total){
-                    if(ygaro * (j - 2) == yellow) {
-                        answer[0] = i;
-                        answer[1] = j;
+        int garo = 0;
+        int sero = 0;
+        boolean flag = false;
+        for(int i=0;i<total;i++){
+            for(int j=0;j<total;j++){
+                if(i >= j && i * j == total){
+                    if(i * 2 + (j - 2) * 2  == brown){
+                        garo = i;
+                        sero = j;
                         flag = true;
+                        break;    
                     }
+                    
                 }
             }
+            if(flag) break;
         }
+        answer[0] = garo;
+        answer[1] = sero;
         return answer;
     }
 }
