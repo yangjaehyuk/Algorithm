55635e207896935bbe2a00144dd4d02a990d74e2
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-02 17:41:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911?language=java) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 0.06 ms
+메모리: 53.8 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 21:03:56
+2025년 01월 02일 17:41:23
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,20 +2,22 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String criteria = Integer.toBinaryString(n);
-        int cnt = 0;
-        for(int i=0;i<criteria.length();i++){
-            if(criteria.charAt(i) == '1') cnt++;
-        }
-        while(true){
-            n += 1;
-            int cnt2 = 0;
-            String cri = Integer.toBinaryString(n);
-            for(int j=0;j<cri.length();j++){
-                if(cri.charAt(j) == '1') cnt2++;
+        // 조건 1
+        for(int i=n+1;i<1000001;i++){
+            // 조건 2
+            int num1 = 0;
+            int nnum = 0;
+            String num1B = Integer.toBinaryString(n);
+            String iB = Integer.toBinaryString(i);
+            for(int j=0;j<num1B.length();j++){
+                if(num1B.charAt(j) == '1') num1++;
             }
-            if(cnt == cnt2) {
-                answer = n;
+            for(int j=0;j<iB.length();j++){
+                if(iB.charAt(j) == '1') nnum++;
+            }
+            // 조건 3
+            if(num1 == nnum){
+                answer = i;
                 break;
             }
         }
