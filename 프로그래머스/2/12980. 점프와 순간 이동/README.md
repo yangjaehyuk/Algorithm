1f89059015546213f8c2543e7e6f24919a69e3e2
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-03 21:36:34 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/README.md
+++ 프로그래머스/2/12980. 점프와 순간 이동/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점프와 순간 이동 - 12980 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=java) 
 
 ### 성능 요약
 
-메모리: 52.5 MB, 시간: 0.02 ms
+메모리: 74.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 09:18:56
+2025년 01월 03일 21:36:32
 
 ### 문제 설명
 
--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,39 +2,34 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-        if(n==1) return 1;
-        if(n%2==0){
+        int answer = 0;
+        if(n == 1) return 1;
+        if(n % 2 == 0){
             while(true){
-                if(n==1) break;
-                if(n%2==0) n/=2;
-                else{
+                if(n == 1) break;
+                if(n % 2 != 0){
                     n--;
-                    ans++;
+                    answer++;
+                }
+                else{
                     n/=2;
                 }
             }
-            ans++;
         }
         else{
             n--;
-            ans++;
+            answer++;
             while(true){
-                if(n==1) break;
-                if(n%2==0) n/=2;
-                else{
+                if(n == 1) break;
+                if(n % 2 != 0){
                     n--;
-                    ans++;
+                    answer++;
+                }
+                else{
                     n/=2;
                 }
             }
-            ans++;
-            
         }
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        // System.out.println("Hello Java");
-
-        return ans;
+        return answer+1;
     }
 }
