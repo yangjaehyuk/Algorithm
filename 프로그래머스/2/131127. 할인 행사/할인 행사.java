da5fe83ba1ac8e8af160c6e81c09a32db9ae7f99
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-04 17:31:00 +0900

    [level 2] Title: 할인 행사, Time: 76.07 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,35 +1,24 @@
 import java.util.*;
 class Solution {
-    static int idx = 0;
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         Map<String, Integer> map = new HashMap<>();
         for(int i=0;i<want.length;i++){
             map.put(want[i], number[i]);
         }
-        for(int i=0;i<discount.length-9;i++){
-            Map<String, Integer> tmp = new HashMap<>();
-            boolean check = true;
-            for(int j=i;j<i+10;j++){
-                // System.out.print(i+" "+j+" ");
-                // 여기서 map에 있는 value가 다 0이 되는 지 확인
-                if(tmp.containsKey(discount[j])==false){
-                    tmp.put(discount[j],1);
+        for(int i=0;i<discount.length - 9;i++){
+            Map<String, Integer> map1 = new HashMap<>();
+            for(int j=i;j<10+i;j++){
+                if(map1.get(discount[j]) == null){
+                    map1.put(discount[j], 1);
                 }
                 else{
-                    tmp.put(discount[j],tmp.get(discount[j])+1);
-                }
-                
-            }
-            // tmp와 map 비교
-            for(int j=0;j<want.length;j++){
-                if((tmp.get(want[j]) != map.get(want[j])) || (!tmp.containsKey(want[j]))){
-                    check = false;
-                    break;
+                    int val = map1.get(discount[j]);
+                    map1.put(discount[j], val + 1);
                 }
             }
-            if(check) answer++;
-            // System.out.println();
+            boolean flag = map.equals(map1);
+            if(flag) answer++;
         }
         return answer;
     }
