883d9df635090cf1b7d3d7113bbf60e745b4d7d5
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-04 16:24:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 0.02 ms
+메모리: 89.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 15:30:57
+2025년 01월 04일 16:24:23
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,26 +1,15 @@
+import java.util.*;
 class Solution
 {
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        
-        while(a!=b){
-            if(a%2!=0){
-                a++;
-            }
-            a/=2;
-            
-            if(b%2!=0){
-                b++;
-            }
-            b/=2;
+        while(true){
+            if(a == b) break;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
             answer++;
-            
-            
         }
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        // System.out.println("Hello Java");
-
         return answer;
     }
 }
