14657c1c3a32186b53fb23a76b7664d98890be84
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-05 20:50:38 +0900

    [level 2] Title: 피로도, Time: 7.97 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 94.6 MB, 시간: 99.27 ms
+메모리: 80 MB, 시간: 7.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 08일 00:49:12
+2025년 01월 05일 20:50:37
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,46 +2,38 @@ import java.util.*;
 class Solution {
     static int[] path;
     static boolean[] visited;
-    static int[] arr;
-    static int answer = 0;
     static int maxi = 0;
     public int solution(int k, int[][] dungeons) {
-        arr = new int[dungeons.length];
+        int answer = -1;
         path = new int[dungeons.length];
         visited = new boolean[dungeons.length];
-        for(int i=0;i<dungeons.length;i++){
-            arr[i] = i;
-        }
-        dfs(k, 0, dungeons.length, dungeons);
+        int myhp = k;
+        dfs(0, dungeons.length, dungeons, myhp);
+        answer = maxi;
         return answer;
     }
-    static void dfs(int k, int lev, int level, int[][] dungeons){
+    
+    public static void dfs(int lev, int level, int[][] dungeons, int myhp){
         if(lev == level){
-            int tmp = k;
-            for(int j=0;j<lev;j++){
-                if(tmp >= dungeons[path[j]][0]){
-                    tmp -= dungeons[path[j]][1];
-                    maxi++;
+            int tmp = 0;
+            for(int i=0;i<path.length;i++){
+                if(dungeons[path[i]][0] <= myhp){
+                    myhp -= dungeons[path[i]][1];
+                    tmp++;
                 }
+                else break;
             }
-            if(maxi >= answer) {
-                answer = maxi;
-                maxi = 0;
-            }
-            else maxi = 0;
-            
-            System.out.println();
+            if(tmp >= maxi) maxi = tmp;
             return;
         }
-        for(int i=0;i<level;i++){
+        
+        for(int i=0;i<dungeons.length;i++){
             if(!visited[i]){
-                path[lev] = arr[i];
                 visited[i] = true;
-                dfs(k, lev+1, level, dungeons);
+                path[lev] = i;
+                dfs(lev+1, level, dungeons, myhp);
                 visited[i] = false;
-                path[lev] = 0;
             }
         }
-        
     }
 }
