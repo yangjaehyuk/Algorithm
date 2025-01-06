672792b9ae4fa676b690818074bff81d3bb6a7f2
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-06 21:16:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.62 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,34 +1,30 @@
 import java.util.*;
 class Solution {
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
     static boolean[][] visited;
-    static int[][] arr;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
     public int solution(int[][] maps) {
         int answer = 0;
         visited = new boolean[maps.length][maps[0].length];
-        arr = new int[maps.length][maps[0].length];
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0,0));
-        visited[0][0]=true;
+        visited[0][0] = true;
+        q.add(new Pair(0, 0));
         while(!q.isEmpty()){
             Pair p = q.poll();
             int x = p.x;
             int y = p.y;
-            
             for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && !visited[nx][ny] && maps[nx][ny] == 1){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length && !visited[nx][ny] && maps[nx][ny] != 0){
+                    maps[nx][ny] = maps[x][y] + 1;
                     q.add(new Pair(nx, ny));
                     visited[nx][ny] = true;
-                    arr[nx][ny] = arr[x][y] + 1;
                 }
             }
         }
-        
-        if(visited[maps.length-1][maps[0].length-1] == false) answer = -1;
-        else answer = arr[maps.length-1][maps[0].length-1] + 1;
+        if(visited[maps.length-1][maps[0].length-1]) answer=maps[maps.length-1][maps[0].length-1];
+        else answer=-1;
         return answer;
     }
     
@@ -36,8 +32,8 @@ class Solution {
         int x;
         int y;
         Pair(int x, int y){
-            this.x = x;
-            this.y = y;
+            this.x=x;
+            this.y=y;
         }
     }
 }
