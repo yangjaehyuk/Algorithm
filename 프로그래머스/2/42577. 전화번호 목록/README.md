3b31c7f58c4adc1e478829d5855b3582a336eef4
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-06 18:07:43 +0900

    [level 2] Title: 전화번호 목록, Time: 232.02 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 99.5 MB, 시간: 232.43 ms
+메모리: 99.8 MB, 시간: 232.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 08일 01:20:35
+2025년 01월 06일 18:07:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        List<String> list = new ArrayList<String>();
-        for(String s: phone_book){
-            list.add(s);
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<phone_book.length;i++){
+            list.add(phone_book[i]);
         }
         Collections.sort(list);
         String cri = list.get(0);
-        for(int i=1;i<list.size();i++){
-            // System.out.println(list.get(i));
+        for(int i=1;i<phone_book.length;i++){
             if(list.get(i).startsWith(cri)) return false;
             cri = list.get(i);
         }
