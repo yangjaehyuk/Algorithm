f643dab7e0eaec9a7876d9020ccd103cfb476eef
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-06 20:53:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 13.43 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,50 +1,45 @@
 import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        List<String> list = new ArrayList<>();
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
-        List<String> Only1 = new ArrayList<>();
         int answer = 0;
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
+        List<String> s1 = new ArrayList<>();
+        List<String> s2 = new ArrayList<>();
         for(int i=0;i<str1.length()-1;i++){
-            StringBuilder sb = new StringBuilder();
-            if('A'<=str1.charAt(i) && str1.charAt(i)<='Z' && 'A'<=str1.charAt(i+1) && str1.charAt(i+1)<='Z'){
-                sb.append(str1.charAt(i));
-                sb.append(str1.charAt(i+1));
-                list.add(sb.toString());
-                list1.add(sb.toString());
-                Only1.add(sb.toString());
-                
+            if(Character.isAlphabetic(str1.charAt(i)) && Character.isAlphabetic(str1.charAt(i+1))){
+                String temp = str1.charAt(i)+""+str1.charAt(i+1);
+                s1.add(temp);
             }
         }
         for(int i=0;i<str2.length()-1;i++){
-            StringBuilder sb = new StringBuilder();
-            if('A'<=str2.charAt(i) && str2.charAt(i)<='Z' && 'A'<=str2.charAt(i+1) && str2.charAt(i+1)<='Z'){
-                sb.append(str2.charAt(i));
-                sb.append(str2.charAt(i+1));
-                list.add(sb.toString());
-                list2.add(sb.toString());
+            if(Character.isAlphabetic(str2.charAt(i)) && Character.isAlphabetic(str2.charAt(i+1))){
+                String temp = str2.charAt(i)+""+str2.charAt(i+1);
+                s2.add(temp);
             }
         }
-        int gop = 0;
-        int hap = 0;
-        for(String s: list2){
-            Only1.remove(s);
+        // 교집합
+        List<String> gyo = new ArrayList<>();
+        List<String> hap = new ArrayList<>();
+        for(int i=0;i<s1.size();i++){
+            if(s2.remove(s1.get(i))){
+                gyo.add(s1.get(i));
+            }
+            hap.add(s1.get(i));
         }
-        // for(String s: Only1){
-        //     System.out.println(s);
-        // }
-        gop = list1.size() - Only1.size();
-        hap = list2.size() + Only1.size();
-        // System.out.println(gop+" "+hap);
-        double bunja = gop;
-        double bunmo = hap;
-        if(bunja==0.0 && bunmo == 0.0) return 65536;
-        double ans = bunja/bunmo;
-        answer = (int)(ans * 65536);
-        // System.out.println(bunja+" "+bunmo+" "+ans+" "+ans*65536);
+        // 합집합
+        for(int i=0;i<s2.size();i++){
+            hap.add(s2.get(i));
+        }
+        int hapL = hap.size();
+        double gyoL = gyo.size();
+        if(gyoL == 0 || hapL == 0) answer = 65536;
+        if(gyoL == 0 && hapL == 0) answer = 65536;
+        else{
+            double res = gyoL / hapL;
+            double temp = res * 65536;
+            answer = (int)temp;
+        }
         return answer;
     }
 }
