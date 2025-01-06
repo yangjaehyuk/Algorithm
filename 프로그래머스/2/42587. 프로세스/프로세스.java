e748c5202a857f7c0d2e076abf8e12a1cef345dd
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-06 18:25:51 +0900

    [level 2] Title: 프로세스, Time: 1.37 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,52 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        List<Integer> ans = new ArrayList<>();
-        List<Integer> lp = new ArrayList<>();
-        for(int i=0;i<priorities.length;i++){
-            lp.add(priorities[i]);
-        }
-        List<Integer> list = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        lp.sort(new Comparator<>(){
-           public int compare(Integer i1, Integer i2){
-               return i2 - i1;
-           } 
-        });
         int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<priorities.length;i++){
+            pq.add(priorities[i]);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<priorities.length;i++){
             map.put(i, priorities[i]);
         }
-        int step = 1;
-        int idx = -1;
-        for(Integer i: map.keySet()){
-            idx++;
-            if(map.get(i)==lp.get(0)){
-                ans.add(i);
-                break;
-            }
-        }
-        // System.out.println(idx); // 시작 인덱스
-        int lpIdx = 1;
+        int idx = 0;
         while(true){
-            if(step==priorities.length) break;
-            else{
-                idx++;
-                if(lp.get(lpIdx) == priorities[idx%priorities.length]){
-                    ans.add(idx%priorities.length);
-                    lpIdx++;
-                    step++;
+            idx %= priorities.length;
+            if(pq.peek() == map.get(idx)){
+                if(location == idx){
+                    break;
                 }
+                answer++;
+                pq.poll();
+                map.remove(idx);
             }
+            idx++;
         }
-        int check = 0;
-        for(Integer i2: ans){
-            check++;
-            if(i2 == location){
-                answer = check;
-                break;
-            }
-        }
-        return answer;
+        return answer + 1;
     }
 }
