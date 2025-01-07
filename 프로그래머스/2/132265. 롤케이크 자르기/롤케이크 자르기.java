19630909f994bdd50613d326b42ce7532381271b
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-07 19:59:15 +0900

    [level 2] Title: 롤케이크 자르기, Time: 427.13 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,38 +1,34 @@
+
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        int len = topping.length;
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
-        for(int i=0;i<len;i++){
-            if(map1.containsKey(topping[i])){
-                int val = map1.get(topping[i]);
-                map1.put(topping[i], val+1);
+        Map<Integer, Integer> bigbro = new HashMap<>();
+        Map<Integer, Integer> littlebro = new HashMap<>();
+        for(int i=0;i<topping.length;i++){
+            if(!bigbro.containsKey(topping[i])){
+                bigbro.put(topping[i], 1);
             }
             else{
-                map1.put(topping[i], 1);
+                int val = bigbro.get(topping[i]) + 1;
+                bigbro.put(topping[i], val);
             }
         }
-        for(int i=0;i<len;i++){
-            int cnt = 0;
-            int now = topping[i];
-            if(map1.containsKey(now)){
-                int val = map1.get(now);
-                map1.put(now, val-1);
-            }
-            if(map1.get(now) <= 0){
-                map1.remove(now);
-            }
-            if(map2.containsKey(topping[i])){
-                int val = map2.get(topping[i]);
-                map2.put(topping[i], val+1);
+        // 동생이 하나 씩 뺏기
+        for(int i=0;i<topping.length;i++){
+            if(!littlebro.containsKey(topping[i])){
+                littlebro.put(topping[i], 1);
             }
             else{
-                map2.put(topping[i], 1);
+                int val = littlebro.get(topping[i]) + 1;
+                littlebro.put(topping[i], val);
             }
-            // System.out.println(map1.size()+" "+map2.size());
-            if(map1.size() == map2.size()) answer++;
+            int val = bigbro.get(topping[i]) - 1;
+            bigbro.put(topping[i], val);
+            if(bigbro.get(topping[i]) == 0) bigbro.remove(topping[i]);
+            int lenbig = bigbro.size();
+            int lenlittle = littlebro.size();
+            if(lenbig == lenlittle) answer++;
         }
         return answer;
     }
