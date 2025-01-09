1f4e9f21d715a107f2fed97d21bf55c2cef8e44b
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-09 23:19:36 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.60 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,17 +1,17 @@
 import java.util.*;
 class Solution {
-    static int big = 1000000007;
     public int solution(int n) {
-        // n = 1 -> 1
-        // n = 2 -> 2
-        // n = 3 -> 3
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i=2;i<n;i++){
-            dp[i] = (dp[i - 1] + dp[i - 2]) % big;
-        }
         int answer = 0;
+        int[] dp = new int[n];
+        if(n == 1) return 1;
+        else if(n == 2) return 2;
+        else{
+            dp[0] = 1;
+            dp[1] = 2;
+            for(int i=2;i<n;i++){
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+            }
+        }
         answer = dp[n - 1];
         return answer;
     }
