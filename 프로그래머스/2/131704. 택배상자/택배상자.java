45a66ff30192aa0603f1aab26f15dcdaf6cc4c13
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-09 22:58:48 +0900

    [level 2] Title: 택배상자, Time: 153.79 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,21 +2,32 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> s = new Stack<>();
-        int idx = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
         for(int i=1;i<=order.length;i++){
-            if(order[idx] != i){
-                s.push(i);
-                continue;
+            dq.add(i);
+        }
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        int idx = 0;
+        while(true){
+            if(!dq.isEmpty()) {
+                int dqpoll = dq.pollFirst();
+                stack.add(dqpoll);
             }
-            idx++;
-            answer++;
-            while(s.size()>0 && s.peek()==order[idx]){
-                s.pop();
-                answer++;
+            if(stack.peek() > order[idx]) dq.addFirst(stack.pop());
+            if(stack.peek() == order[idx]){
                 idx++;
+                if (idx >= order.length) {
+                    return list.size() + 1;
+                }
+                stack.pop();
+                list.add(order[idx]);
+            }
+            else{
+                if(stack.peek() > order[idx]) break;
             }
         }
+        answer = list.size();
         return answer;
     }
 }
