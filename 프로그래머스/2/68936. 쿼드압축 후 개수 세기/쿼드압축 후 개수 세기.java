708f948d1f8441c1ae9510e3df5a62ccfd9f36b7
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-13 19:41:35 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.31 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,37 +1,36 @@
+import java.util.*;
 class Solution {
-    static int num1 = 0;
-    static int num0 = 0;
+    static int one = 0;
+    static int zero = 0;
     public int[] solution(int[][] arr) {
-        int[] answer = new int[2];
-        int m = arr.length;
-        dfs(0, 0, m, arr);
-        answer[0] = num0;
-        answer[1] = num1;
+        int[] answer = {};
+        answer = new int[2];
+        dfs(0, 0, arr.length, arr);
+        answer[0] = zero;
+        answer[1] = one;
         return answer;
     }
     
     public static void dfs(int x, int y, int len, int[][] arr){
         if(check(x, y, len, arr)){
-            if(arr[x][y] == 1) num1++;
-            else num0++;
+            if(arr[x][y] == 1) one++;
+            else zero++;
             return;
         }
-        len/=2;
+        len /= 2;
         dfs(x, y, len, arr);
-        dfs(x, y+len, len, arr);
         dfs(x+len, y, len, arr);
+        dfs(x, y+len, len, arr);
         dfs(x+len, y+len, len, arr);
-        
     }
     
     public static boolean check(int x, int y, int len, int[][] arr){
         int cri = arr[x][y];
         for(int i=x;i<x+len;i++){
             for(int j=y;j<y+len;j++){
-                if(cri != arr[i][j]) return false;
+                if(arr[i][j] != cri) return false;
             }
         }
         return true;
     }
-    
 }
