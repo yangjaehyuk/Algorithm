34d42325e2a16fd1212d587afbcdc70e4ecb13e9
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-13 17:54:09 +0900

    [level 2] Title: 가장 큰 수, Time: 3135.03 ms, Memory: 409 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 133 MB, 시간: 274.41 ms
+메모리: 409 MB, 시간: 3135.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 22:47:29
+2025년 01월 13일 17:54:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,24 +1,25 @@
 import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
-        StringBuilder answer = new StringBuilder();
-        String[] ans = new String[numbers.length];
-
-        for(int i = 0; i < numbers.length; i++) {
-            ans[i] = Integer.toString(numbers[i]);
+        String answer = "";
+        List<String> list = new ArrayList<>();
+        boolean flag = false;
+        for(int i=0;i<numbers.length;i++){
+            list.add(String.valueOf(numbers[i]));
+            if(numbers[i] == 0) flag = true;
+            else flag = false;
         }
-
-        Arrays.sort(ans, (a, b) -> (b + a).compareTo(a + b));
-
-        for (String num : ans) {
-            answer.append(num);
+        if(flag) return "0";
+        Collections.sort(list, new Comparator<>() {
+           @Override
+            public int compare(String s1, String s2){
+                return (s2 + s1).compareTo(s1 + s2);
+            }
+        });
+        
+        for(int i=0;i<list.size();i++){
+            answer+=list.get(i);
         }
-
-        if (answer.charAt(0) == '0') {
-            return "0";
-        }
-
-        return answer.toString();
+        return answer;
     }
 }
