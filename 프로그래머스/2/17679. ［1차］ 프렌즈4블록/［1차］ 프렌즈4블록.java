e9b15a2f4b7b2b090cb6f7243acb57c0128ab08d
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-14 00:31:14 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 66.07 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,74 +1,70 @@
 import java.util.*;
 class Solution {
+    static int[] dx = {1, 1, 0};
+    static int[] dy = {0, 1, 1};
     public int solution(int m, int n, String[] board) {
-        int[] dx = {1,0,1,0};
-        int[] dy = {0,1,1,0};
         int answer = 0;
         char[][] arr = new char[m][n];
-        int step = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 arr[i][j] = board[i].charAt(j);
             }
         }
-        while(true){
-            step++;
-            if(step==300) break;
+        for(int i=0;i<500;i++){
             boolean[][] visited = new boolean[m][n];
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    boolean flag = true;
+            for(int j=0;j<m;j++){
+                for(int k=0;k<n;k++){
+                    char now = arr[j][k];
+                    if(now == ' ' || now >= 'a' && now <= 'z') continue;
                     int cnt = 0;
-                    for(int k=0;k<4;k++){
-                        int nx = i+dx[k];
-                        int ny = j+dy[k];
-                        if(nx >=0 && nx < m && ny>=0 && ny<n){
-                            if(arr[i][j] != arr[nx][ny] || arr[i][j] == ' '){
-                                flag = false;
-                                break;
-                            }
-                            else cnt++;
-                        }
+                    for(int l=0;l<3;l++){
+                        int nx = j + dx[l];
+                        int ny = k + dy[l];
+                        if(nx >= 0 && nx < m && ny >= 0 && ny < n && arr[nx][ny] == now) cnt++;
                     }
-                    if(flag && cnt == 4){
-                        for(int l=0;l<4;l++){
-                            int nx = i+dx[l];
-                            int ny = j+dy[l];
-                            if(nx >= 0 && nx < m && ny >= 0 && ny < n){
-                                visited[nx][ny] = true;
-                            }
-                        }
+                    if(cnt == 3){
+                        visited[j][k] = true;
+                        visited[j+1][k] = true;
+                        visited[j+1][k+1] = true;
+                        visited[j][k+1] = true;
                     }
                 }
             }
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(visited[i][j] == true){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<n;k++){
+                    // System.out.print(visited[j][k]+" ");
+                    if(visited[j][k]){
                         answer++;
-                        arr[i][j] = ' ';
+                        arr[j][k] = ' ';
                     }
                 }
             }
-            for(int i=m-1;i>=0;i--){
-                for(int j=0;j<n;j++){
-                    if(arr[i][j] == ' '){
-                        for(int k=i-1;k>=0;k--){
-                            if (arr[k][j] != ' ') {
-                                    arr[i][j] = arr[k][j];
-                                    arr[k][j] = ' ';
-                                    break;
-                            }
+            // for(int j=0;j<m;j++){
+            //     for(int k=0;k<n;k++){
+            //         System.out.print(arr[j][k]+" ");
+            //     }
+            //     System.out.println();
+            // }
+            for(int j=0;j<m;j++){
+                for(int k=0;k<n;k++){
+                    if(arr[j][k] == ' '){
+                        for(int l=j;l>0;l--){
+                            arr[l][k] = arr[l-1][k];
+                            arr[l-1][k] = ' ';
                         }
+                        
                     }
                 }
             }
+            // System.out.println("---------------------");
+            // for(int j=0;j<m;j++){
+            //     for(int k=0;k<n;k++){
+            //         System.out.print(arr[j][k]+" ");
+            //     }
+            //     System.out.println();
+            // }
+            
         }
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 System.out.print(arr[i][j]+" ");
-//             }
-//             System.out.println();
-//         }
         return answer;
     }
 }
