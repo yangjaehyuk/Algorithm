df3c0e19759e48841ecee0caebde05c4676bea9a
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-30 22:34:49 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 10.36 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -2,26 +2,49 @@ import java.util.*;
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-        while(storey!=0){
-            int first = storey%10; //1의 자리
-            int second = (storey/10)%10; // 10의 자리 
-            if(first<5){
-                answer+=first;
-            }
-            else if(first==5){
-                if(second<5){
-                    answer+=first;
-                }
-                else{
-                    answer+=10-first;
-                    storey+=10;
-                }
+        Stack<Integer> stack = new Stack<>();
+        String tmp = String.valueOf(storey);
+        for(int i=0;i<tmp.length();i++){
+            stack.add(tmp.charAt(i) - '0');
+        }
+        boolean flag = false;
+        while(!stack.isEmpty()){
+            int x = stack.pop();
+            if(flag) x+=1;
+            if(x < 5){
+                answer+=x;
+                flag = false;
             }
             else{
-                answer+=10-first;
-                storey+=10;
+                if(x == 5){
+                    if(!stack.isEmpty() && stack.peek() < 5){
+                        answer +=5;
+                        flag = false;
+                    }
+                    else{
+                        flag=true;
+                        answer+=5;
+                    }
+                }
+                else if(x > 9){
+                    // System.out.println(len+" "+stack.size()+" "+x);
+                    if(stack.size() == 0) answer++;
+                    // answer += x - 10;
+                    flag = true;
+                }
+                else{
+                    if(stack.size() == 0) {
+                        System.out.println(x+" "+answer);
+                        if(x > 5) answer += 10 - x + 1;
+                        else answer += x;
+                    }
+                    else{
+                        
+                        flag = true;
+                        answer+=10-x;
+                    }
+                }
             }
-            storey/=10;
         }
         return answer;
     }
