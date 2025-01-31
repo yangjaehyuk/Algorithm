ee127515943540507872d93452a923cbde332a00
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-01-31 17:08:01 +0900

    [level 2] Title: 리코쳇 로봇, Time: 5.20 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,8 +2,11 @@ import java.util.*;
 class Solution {
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visited;
     public int solution(String[] board) {
         int answer = 0;
+        Queue<Pair> q = new LinkedList<>();
+        visited = new boolean[board.length][board[0].length()];
         int startx = 0;
         int starty = 0;
         int endx = 0;
@@ -17,54 +20,51 @@ class Solution {
                 else if(board[i].charAt(j) == 'G'){
                     endx = i;
                     endy = j;
-                    
                 }
             }
         }
-        int[][] graph = new int[board.length][board[0].length()];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length();j++){
-                graph[i][j] = 123456789;
-            }
-        }
-        graph[startx][starty] = 0;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(startx, starty));
+        q.add(new Pair(startx, starty, 0));
+        visited[startx][starty] = true;
         while(!q.isEmpty()){
             Pair p = q.poll();
             int x = p.x;
             int y = p.y;
-            for(int j=0;j<4;j++){
+            int cnt = p.cnt;
+            if(x == endx && y == endy) {
+                answer = cnt;
+                break;
+            }
+            for(int i=0;i<4;i++){
                 int nx = x;
                 int ny = y;
                 while(true){
-                    if(nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length() && board[nx].charAt(ny) != 'D'){
-                        nx += dx[j];
-                        ny += dy[j];
-                    }
-                    else{
-                        nx -= dx[j];
-                        ny -= dy[j];
+                    nx+=dx[i];
+                    ny+=dy[i];
+                    if(nx<0||ny<0||nx>=board.length||ny>=board[0].length()||board[nx].charAt(ny) == 'D'){
+                        nx -= dx[i];
+                        ny-=dy[i];
                         break;
                     }
                 }
-                if (graph[x][y] + 1 < graph[nx][ny]) {
-                    graph[nx][ny] = graph[x][y] + 1;
-                    q.add(new Pair(nx, ny));
+                if(!visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    q.add(new Pair(nx, ny, cnt+1));
                 }
                 
             }
         }
-        if(graph[endx][endy] == 123456789) answer= -1;
-        else answer = graph[endx][endy];
+        if(!visited[endx][endy]) answer = -1;
         return answer;
     }
+    
     public static class Pair{
         int x;
         int y;
-        Pair(int x, int y){
+        int cnt;
+        Pair(int x, int y, int cnt){
             this.x = x;
             this.y = y;
+            this.cnt = cnt;
         }
     }
 }
