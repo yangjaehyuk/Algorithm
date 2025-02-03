cbbafd404d09909c9eebf72e18f2eea5e47dd93b
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-02-03 15:06:07 +0900

    [level 2] Title: 미로 탈출, Time: 6.38 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,99 +1,97 @@
+
 import java.util.*;
 class Solution {
-    static boolean[][] visited;
-    static char[][] arr;
-    static int[][] ans;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visited;
     public int solution(String[] maps) {
-        int n = maps.length;
-        int m = maps[0].length();
+        int answer = 0;
+        char[][] arr = new char[maps.length][maps[0].length()];
         int startx = 0;
         int starty = 0;
-        int rebelx = 0;
-        int rebely = 0;
+        int lebelx = 0;
+        int lebely = 0;
         int endx = 0;
         int endy = 0;
-        ans = new int[n][m];
-        arr = new char[n][m];
-        visited = new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        boolean check1 = false;
+        int tmp = 0;
+        for(int i=0;i<maps.length;i++){
+            for(int j=0;j<maps[i].length();j++){
                 arr[i][j] = maps[i].charAt(j);
-                if(maps[i].charAt(j) == 'S'){
+                if(arr[i][j] == 'S'){
                     startx = i;
                     starty = j;
                 }
-                else if(maps[i].charAt(j) == 'L'){
-                    rebelx = i;
-                    rebely = j;
+                else if(arr[i][j] == 'L'){
+                    lebelx = i;
+                    lebely = j;
                 }
-                else if(maps[i].charAt(j) == 'E'){
+                else if(arr[i][j] == 'E'){
                     endx = i;
                     endy = j;
                 }
             }
         }
-        boolean checkRebel = false;
-        boolean checkEnd = false;
+        visited = new boolean[maps.length][maps[0].length()];
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(startx, starty));
+        q.add(new Pair(startx, starty, 0));
         visited[startx][starty] = true;
         while(!q.isEmpty()){
             Pair p = q.poll();
             int x = p.x;
             int y = p.y;
-            if(x == rebelx && y == rebely) {
-                checkRebel = true;
+            int cnt = p.cnt;
+            if(arr[x][y] == 'L') {
+                check1 = true;
+                tmp = cnt;
                 break;
             }
             for(int i=0;i<4;i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && arr[nx][ny] != 'X'){
-                    q.add(new Pair(nx, ny));
+                if(nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length() && arr[nx][ny] != 'X' && !visited[nx][ny]){
                     visited[nx][ny] = true;
-                    ans[nx][ny] = ans[x][y] + 1;
+                    q.add(new Pair(nx, ny, cnt + 1));
                 }
             }
         }
-        if(checkRebel){
+        if(!check1) return -1;
+        else{
             while(!q.isEmpty()) q.poll();
-            q.add(new Pair(rebelx, rebely));
-            boolean[][] visited2 = new boolean[n][m];
-            visited2[rebelx][rebely] = true;
+            q.add(new Pair(lebelx, lebely, tmp));
+            visited = new boolean[maps.length][maps[0].length()];
+            visited[lebelx][lebely] = true;
             while(!q.isEmpty()){
                 Pair p = q.poll();
                 int x = p.x;
                 int y = p.y;
-                if(x == endx && y == endy) {
-                    checkEnd = true;
-                    break;
+                int cnt = p.cnt;
+                if(arr[x][y] == 'E') {
+                    return cnt;
                 }
                 for(int i=0;i<4;i++){
                     int nx = x + dx[i];
                     int ny = y + dy[i];
-                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited2[nx][ny] && arr[nx][ny] != 'X'){
-                        q.add(new Pair(nx, ny));
-                        visited2[nx][ny] = true;
-                        ans[nx][ny] = ans[x][y] + 1;
+                    if(nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length() && arr[nx][ny] != 'X' && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        q.add(new Pair(nx, ny, cnt + 1));
                     }
                 }
             }
         }
-        else return -1;
-        int answer = 0;
-        if(checkEnd) answer = ans[endx][endy];
-        else return -1;
+        
+        if(!visited[endx][endy]) return -1;
         return answer;
     }
     
     public static class Pair{
         int x;
         int y;
-        Pair(int x, int y){
+        int cnt;
+        Pair(int x, int y, int cnt){
             this.x = x;
             this.y = y;
+            this.cnt = cnt;
         }
     }
 }
