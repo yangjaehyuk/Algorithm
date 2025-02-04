a776ace6a59e8895dff0ccd06adfbeb2e892744e
Jaehyuk Yang <37584686+yangjaehyuk@users.noreply.github.com>
2025-02-04 18:45:18 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 29.79 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -2,72 +2,62 @@ import java.util.*;
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        int maxA = Arrays.stream(arrayA).max().getAsInt();
-        int maxB = Arrays.stream(arrayB).max().getAsInt();
-        int finalMax = Math.max(maxA, maxB);
-        int div = 2;
-        while(true){
-            if(div > finalMax) break;
-            boolean check1 = true;
-            boolean check2 = true;
+        boolean flagA = true;
+        boolean flagB = true;
+        int num1 = gcdgcd(arrayA); // arrayB에선 다 나누어 떨어지면 안됨
+        int num2 = gcdgcd(arrayB); // arrayA에선 다 나누어 떨어지면 안됨
+        if(num1 == 1){
             for(int i=0;i<arrayA.length;i++){
-                if(arrayA[i] % div != 0){
-                    check1 = false;
+                if(arrayA[i] % num2 == 0){
+                    flagB = false;
                     break;
                 }
             }
-            for(int j=0;j<arrayB.length;j++){
-                if(arrayB[j] % div != 0){
-                    check2 = false;
+            if(flagB) answer = num2;
+            else answer = 0;
+        }
+        else if(num2 == 1){
+            for(int i=0;i<arrayB.length;i++){
+                if(arrayB[i] % num1 == 0){
+                    flagA = false;
                     break;
                 }
             }
-            if(check1){
-                list1.add(div);
-            }
-            if(check2){
-                list2.add(div);
-            }
-            div++;
+            if(flagA) answer = num1;
+            else answer = 0;
         }
-        Collections.sort(list1);
-        Collections.sort(list2);
-        for(Integer item: list1){
-            System.out.println("this is l1"+" "+item);
-        }
-        for(Integer item: list2){
-            System.out.println("this is l2"+" "+item);
-        }
-        for(int i=0;i<list1.size();i++){
-            boolean checkL1 = false;
-            for(int j=0;j<arrayB.length;j++){
-                if(arrayB[j] % list1.get(i) == 0){
-                    checkL1 = true;
-                    break;
-                }
-            }
-            if(checkL1 == false) ans.add(list1.get(i)); 
-        }
-        for(int i=0;i<list2.size();i++){
-            boolean checkL2 = false;
-            for(int j=0;j<arrayA.length;j++){
-                if(arrayA[j] % list2.get(i) == 0){
-                    checkL2 = true;
-                    break;
-                }
-            }
-            if(checkL2 == false) ans.add(list2.get(i));
-        }
-        Collections.sort(ans);
-        for(int i=0;i<ans.size();i++){
-            System.out.println("this is ans "+ans.get(i));
-        }
-        if(ans.size() == 0) return 0;
         else{
-            return ans.get(ans.size()-1);
+            for(int i=0;i<arrayA.length;i++){
+                if(arrayA[i] % num2 == 0){
+                    flagB = false;
+                    break;
+                }
+            }
+            for(int i=0;i<arrayB.length;i++){
+                if(arrayB[i] % num1 == 0){
+                    flagA = false;
+                    break;
+                }
+            }
+
+            if(flagA == true && flagB == true) answer = Math.max(num1, num2);
+            else if(flagA) answer = num1;
+            else if(flagB) answer = num2;
+            else answer = 0;
         }
+        
+        return answer;
+    }
+    
+    private int gcdgcd(int[] arr){
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = gcd(result, arr[i]);
+        }
+        return result;
+    }
+    private int gcd(int first, int second){
+        if(first % second == 0) return second;
+        return gcd(second, first % second);
     }
 }
